fd52fd65e7b352822bdc79d3ba5b02fe8f2f7cc5
jeffreypalermo <jeffreypalermo@eb32e4ba-b140-0410-9c0e-b965c4cb1785>
2007-12-11 05:58:15 +0000

    #454 - applied patch from wcpierce with WindsorControllerFactory - added 1.0.3 Castle distribution to /bin

---
+++ trunk/src/MVCContrib/ControllerFactories/WindsorControllerFactory.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Web.Mvc;
+using Castle.Windsor;
+
+namespace MVCContrib.ControllerFactories
+{
+	public class WindsorControllerFactory : IControllerFactory
+	{
+		IController IControllerFactory.CreateController(RequestContext context, Type controllerType)
+		{
+			IWindsorContainer container = ObtainContainer(context);
+			IController controller = GetController<IController>(container, controllerType);
+			return controller;
+		}
+
+		protected virtual IWindsorContainer ObtainContainer(RequestContext context)
+		{
+			if( context == null )
+			{
+				throw new ArgumentNullException("context");
+			}
+
+			IContainerAccessor containerAccessor = context.HttpContext.ApplicationInstance as IContainerAccessor;
+			if (containerAccessor == null)
+			{
+				throw new InvalidOperationException("You must extend the HttpApplication in your web project and implement the IContainerAccessor to properly expose your container instance");
+			}
+			
+			IWindsorContainer container = containerAccessor.Container;
+			if (container == null)
+			{
+				throw new InvalidOperationException("The container seems to be unavailable in your HttpApplication subclass");
+			}
+
+			return container;
+		}
+
+		protected virtual T GetController<T>(IWindsorContainer container, Type controllerType) where T : IController
+		{
+			if( container == null )
+			{
+				throw new ArgumentNullException("container");
+			}
+			if( controllerType == null )
+			{
+				throw new ArgumentNullException("controllerType");
+			}
+
+			return (T)container.Resolve(controllerType);
+		}
+	}
+}
