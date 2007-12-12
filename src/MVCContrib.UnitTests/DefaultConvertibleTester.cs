2879fe1acd8d90562da2e16c215d8295bee7d6b5
jeffreypalermo <jeffreypalermo@eb32e4ba-b140-0410-9c0e-b965c4cb1785>
2007-12-12 05:08:38 +0000

    #519 - applied patch from wcpierce.  Patch increased test coverage.  TD.Net with NCover is an easy way to guage test coverage.

--- src/MVCContrib.UnitTests/DefaultConvertibleTester.cs
+++ src/MVCContrib.UnitTests/DefaultConvertibleTester.cs
@@ -28,55 +28,119 @@ namespace MVCContrib.UnitTests
 		[Test]
 		public void CanConvertSByte()
 		{
-			DefaultConvertible convertible = new DefaultConvertible("1");
+			DefaultConvertible convertible = new DefaultConvertible(sbyte.MaxValue.ToString());
 			sbyte value = convertible.ToSByte(CultureInfo.InvariantCulture);
 
-			Assert.AreEqual(1, value);
+			Assert.AreEqual(sbyte.MaxValue, value);
 		}
 
 		[Test]
 		public void CanConvertByte()
 		{
-			DefaultConvertible convertible = new DefaultConvertible("1");
+			DefaultConvertible convertible = new DefaultConvertible(byte.MaxValue.ToString());
 			byte value = convertible.ToByte(CultureInfo.InvariantCulture);
 
-			Assert.AreEqual(1, value);
+			Assert.AreEqual(byte.MaxValue, value);
 		}
 
 		[Test]
 		public void CanConvertShort()
 		{
-			DefaultConvertible convertible = new DefaultConvertible("1");
+			DefaultConvertible convertible = new DefaultConvertible(short.MaxValue.ToString());
 			short value = convertible.ToInt16(CultureInfo.InvariantCulture);
 
-			Assert.AreEqual(1, value);
+			Assert.AreEqual(short.MaxValue, value);
 		}
 
 		[Test]
 		public void CanConvertUShort()
 		{
-			DefaultConvertible convertible = new DefaultConvertible("1");
+			DefaultConvertible convertible = new DefaultConvertible(ushort.MaxValue.ToString());
 			ushort value = convertible.ToUInt16(CultureInfo.InvariantCulture);
 
-			Assert.AreEqual(1, value);
+			Assert.AreEqual(ushort.MaxValue, value);
 		}
 
 		[Test]
 		public void CanConvertInt()
 		{
-			DefaultConvertible convertible = new DefaultConvertible("1");
+			DefaultConvertible convertible = new DefaultConvertible(int.MaxValue.ToString());
 			int value = convertible.ToInt32(CultureInfo.InvariantCulture);
 
-			Assert.AreEqual(1, value);
+			Assert.AreEqual(int.MaxValue, value);
 		}
 
 		[Test]
 		public void CanConvertUInt()
 		{
-			DefaultConvertible convertible = new DefaultConvertible("1");
+			DefaultConvertible convertible = new DefaultConvertible(uint.MaxValue.ToString());
 			uint value = convertible.ToUInt32(CultureInfo.InvariantCulture);
 
-			Assert.AreEqual(1, value);
+			Assert.AreEqual(uint.MaxValue, value);
+		}
+
+		[Test]
+		public void CanConvertLong()
+		{
+			DefaultConvertible convertible = new DefaultConvertible(long.MaxValue.ToString());
+			long value = convertible.ToInt64(CultureInfo.InvariantCulture);
+
+			Assert.AreEqual(long.MaxValue, value);
+		}
+
+		[Test]
+		public void CanConvertULong()
+		{
+			DefaultConvertible convertible = new DefaultConvertible(ulong.MaxValue.ToString());
+			ulong value = convertible.ToUInt64(CultureInfo.InvariantCulture);
+
+			Assert.AreEqual(ulong.MaxValue, value);
+		}
+
+		[Test]
+		public void CanConvertFloat()
+		{
+			DefaultConvertible convertible = new DefaultConvertible("1.1");
+			float value = convertible.ToSingle(CultureInfo.InvariantCulture);
+
+			Assert.AreEqual(1.1F, value);
+		}
+
+		[Test]
+		public void CanConvertDouble()
+		{
+			DefaultConvertible convertible = new DefaultConvertible("1.2");
+			double value = convertible.ToDouble(CultureInfo.InvariantCulture);
+
+			Assert.AreEqual(1.2, value);
+		}
+
+		[Test]
+		public void CanConvertDecimal()
+		{
+			DefaultConvertible convertible = new DefaultConvertible("1.3");
+			decimal value = convertible.ToDecimal(CultureInfo.InvariantCulture);
+
+			Assert.AreEqual(1.3M, value);
+		}
+
+		[Test]
+		public void CanConvertDateTime()
+		{
+			DefaultConvertible convertible = new DefaultConvertible("11/05/1605");
+			DateTime value = convertible.ToDateTime(CultureInfo.InvariantCulture);
+			DateTime actual = new DateTime(1605, 11, 5);
+
+			Assert.AreEqual(actual, value);
+		}
+
+		[Test]
+		public void ReturnsCorrectTypeCode()
+		{
+			DefaultConvertible convertible = new DefaultConvertible(string.Empty);
+			TypeCode value = convertible.GetTypeCode();
+
+			Assert.AreEqual(TypeCode.String, value);
 		}
 
 		private enum WeAre
@@ -107,6 +171,15 @@ namespace MVCContrib.UnitTests
 		}
 
 		[Test]
+		public void EnumDefaultsToDefaultWhenNull()
+		{
+			DefaultConvertible convertible = new DefaultConvertible(null);
+			WeAre value = (WeAre)convertible.ToEnum(typeof(WeAre));
+
+			Assert.AreEqual(WeAre.Lovin, value);
+		}
+
+		[Test]
 		public void CanConvertGuid()
 		{
 			Guid actual = Guid.NewGuid();
@@ -117,10 +190,39 @@ namespace MVCContrib.UnitTests
 		}
 
 		[Test]
+		public void GuidDefaultsToEmpty()
+		{
+			DefaultConvertible convertible = new DefaultConvertible("GetSome");
+			Guid value = convertible.ToGuid();
+
+			Assert.AreEqual(Guid.Empty, value);
+		}
+
+		[Test]
+		public void GuidDefaultsToEmptyWhenNull()
+		{
+			DefaultConvertible convertible = new DefaultConvertible(null);
+			Guid value = convertible.ToGuid();
+
+			Assert.AreEqual(Guid.Empty, value);
+		}
+
+		[Test]
+		public void FallsBackToTypeConverter()
+		{
+			DefaultConvertible convertible = new DefaultConvertible("1.02:03:04.005");
+			TimeSpan value = (TimeSpan)convertible.ToType(typeof(TimeSpan), CultureInfo.InvariantCulture);
+			TimeSpan actual = new TimeSpan(1, 2, 3, 4, 5);
+
+			Assert.AreEqual(actual, value);
+		}
+
+		[Test]
 		public void NeverThrows()
 		{
 			DefaultConvertible convertible = new DefaultConvertible(null);
 
+			convertible.ToChar(CultureInfo.InvariantCulture);
 			convertible.ToBoolean(CultureInfo.InvariantCulture);
 			convertible.ToByte(CultureInfo.InvariantCulture);
 			convertible.ToDateTime(CultureInfo.InvariantCulture);
