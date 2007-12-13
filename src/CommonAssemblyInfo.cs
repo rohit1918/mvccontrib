5d28bc55ab212ed4eb236594530a8ddd13c77259
mvccontrib <mvccontrib@eb32e4ba-b140-0410-9c0e-b965c4cb1785>
2007-12-13 00:28:08 +0000

    CCNET build 5

--- src/CommonAssemblyInfo.cs
+++ src/CommonAssemblyInfo.cs
@@ -13,11 +13,11 @@ using System.Runtime.InteropServices;
 //------------------------------------------------------------------------------
 
 [assembly: ComVisibleAttribute(false)]
-[assembly: AssemblyVersionAttribute("0.0.1.0")]
-[assembly: AssemblyFileVersionAttribute("0.0.1.0")]
+[assembly: AssemblyVersionAttribute("0.0.1.5")]
+[assembly: AssemblyFileVersionAttribute("0.0.1.5")]
 [assembly: AssemblyCopyrightAttribute("Copyright © MVC Contrib 2007-2007")]
 [assembly: AssemblyProductAttribute("MVCContrib")]
 [assembly: AssemblyCompanyAttribute("MVC Contrib")]
 [assembly: AssemblyConfigurationAttribute("release")]
-[assembly: AssemblyInformationalVersionAttribute("0.0.1.0")]
+[assembly: AssemblyInformationalVersionAttribute("0.0.1.5")]
 
