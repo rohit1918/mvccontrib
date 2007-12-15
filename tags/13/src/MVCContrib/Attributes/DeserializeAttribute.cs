9e73e52afff9b7722455e32656563d5a8dacadfc
mvccontrib <mvccontrib@eb32e4ba-b140-0410-9c0e-b965c4cb1785>
2007-12-15 19:05:40 +0000

    CCNET build 13

---
+++ tags/13/src/MVCContrib/Attributes/DeserializeAttribute.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace MVCContrib.Attributes
+{
+	[AttributeUsage(AttributeTargets.Parameter, AllowMultiple = false, Inherited = false)]
+	public class DeserializeAttribute : Attribute
+	{
+		private readonly string _prefix;
+
+		public DeserializeAttribute(string prefix)
+		{
+			_prefix = prefix;
+		}
+
+		public string Prefix
+		{
+			get { return _prefix; }
+		}
+	}
+}
