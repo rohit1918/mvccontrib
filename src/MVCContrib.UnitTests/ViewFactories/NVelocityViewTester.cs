456dac531f1f6b10e69a6fc73d00248c78a28fad
wcpierce <wcpierce@eb32e4ba-b140-0410-9c0e-b965c4cb1785>
2007-12-16 08:20:59 +0000

    A ViewFactory that uses the NVelocity Template Engine.  Original code is from Hammett, http://hammett.castleproject.org/?p=229, adapted and covered with unit tests.

---
+++ src/MVCContrib.UnitTests/ViewFactories/NVelocityViewTester.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Web;
+using System.Web.Mvc;
+using MvcContrib.ViewFactories;
+using NUnit.Framework;
+using Rhino.Mocks;
+
+namespace MVCContrib.UnitTests.ViewFactories
+{
+	[TestFixture]
+	public class NVelocityViewTester
+	{
+		private NVelocityViewFactory _factory;
+		private ControllerContext _controllerContext;
+		private MockRepository _mocks;
+		private StringWriter _output;
+
+		[SetUp]
+		public void SetUp()
+		{
+			string viewPath = "MVCContrib.UnitTests.ViewFactories";
+
+			IDictionary properties = new Hashtable();
+			properties["resource.loader"] = "assembly";
+			properties["assembly.resource.loader.class"] = "NVelocity.Runtime.Resource.Loader.AssemblyResourceLoader, NVelocity";
+			properties["assembly.resource.loader.assembly"] = "MVCContrib.UnitTests";
+			properties["master.folder"] = viewPath;
+			_factory = new NVelocityViewFactory(properties);
+
+			_output = new StringWriter();
+
+			_mocks = new MockRepository();
+			IHttpContext httpContext = _mocks.DynamicMock<IHttpContext>();
+			IHttpResponse httpResponse = _mocks.DynamicMock<IHttpResponse>();
+			IHttpSessionState httpSessionState = _mocks.DynamicMock<IHttpSessionState>();
+			Expect.Call(httpContext.Session).Repeat.Any().Return(httpSessionState);
+			Expect.Call(httpContext.Response).Repeat.Any().Return(httpResponse);
+			Expect.Call(httpResponse.Output).Repeat.Any().Return(_output);
+
+			RequestContext requestContext = new RequestContext(httpContext, new RouteData());
+			IController controller = _mocks.DynamicMock<IController>();
+
+			_controllerContext = new ControllerContext(requestContext, controller);
+			_controllerContext.RouteData.Values.Add("controller", viewPath);
+		}
+
+		[Test]
+		public void CanRenderView()
+		{
+			IView view = _factory.CreateView(_controllerContext, "view", null, new Hashtable());
+
+			_mocks.ReplayAll();
+
+			ViewContext viewContext = new ViewContext(_controllerContext, new Hashtable(), new TempDataDictionary(_controllerContext.HttpContext));
+
+			view.RenderView(viewContext);
+
+			Assert.AreEqual("View Template", _output.ToString());
+		}
+
+		[Test]
+		public void CanRenderViewWithMaster()
+		{
+			IView view = _factory.CreateView(_controllerContext, "view", "master", new Hashtable());
+
+			_mocks.ReplayAll();
+
+			ViewContext viewContext = new ViewContext(_controllerContext, new Hashtable(), new TempDataDictionary(_controllerContext.HttpContext));
+
+			view.RenderView(viewContext);
+
+			Assert.AreEqual("Master Template View Template", _output.ToString());
+		}
+
+		[Test]
+		public void CanRenderViewWithViewData()
+		{
+			Dictionary<string, object> viewData = new Dictionary<string, object>();
+			viewData["test"] = "test";
+
+			IView view = _factory.CreateView(_controllerContext, "view", null, viewData);
+
+			_mocks.ReplayAll();
+
+			ViewContext viewContext = new ViewContext(_controllerContext, new Hashtable(), new TempDataDictionary(_controllerContext.HttpContext));
+
+			view.RenderView(viewContext);
+
+			Assert.AreEqual("View Template test", _output.ToString());
+		}
+	}
+}
