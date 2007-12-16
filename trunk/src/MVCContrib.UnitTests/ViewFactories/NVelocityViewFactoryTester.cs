b7b86c03f1bedafc13c9029600f1ae869d8e0902
wcpierce <wcpierce@eb32e4ba-b140-0410-9c0e-b965c4cb1785>
2007-12-16 08:20:59 +0000

    A ViewFactory that uses the NVelocity Template Engine.  Original code is from Hammett, http://hammett.castleproject.org/?p=229, adapted and covered with unit tests.

---
+++ trunk/src/MVCContrib.UnitTests/ViewFactories/NVelocityViewFactoryTester.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections;
+using System.Web;
+using System.Web.Mvc;
+using MvcContrib.ViewFactories;
+using NUnit.Framework;
+using NVelocity.Runtime;
+using Rhino.Mocks;
+
+namespace MVCContrib.UnitTests.ViewFactories
+{
+	[TestFixture]
+	public class NVelocityViewFactoryTester
+	{
+		private NVelocityViewFactory _factory;
+		private ControllerContext _controllerContext;
+
+		[SetUp]
+		public void SetUp()
+		{
+			string viewPath = "MVCContrib.UnitTests.ViewFactories";
+
+			IDictionary properties = new Hashtable();
+			properties["resource.loader"] = "assembly";
+			properties["assembly.resource.loader.class"] = "NVelocity.Runtime.Resource.Loader.AssemblyResourceLoader, NVelocity";
+			properties["assembly.resource.loader.assembly"] = "MVCContrib.UnitTests";
+			properties["master.folder"] = viewPath;
+			_factory = new NVelocityViewFactory(properties);
+
+			MockRepository mocks = new MockRepository();
+			IHttpContext httpContext = mocks.DynamicMock<IHttpContext>();
+			RequestContext requestContext = new RequestContext(httpContext, new RouteData());
+			IController controller = mocks.DynamicMock<IController>();
+			
+			_controllerContext = new ControllerContext(requestContext, controller);
+			_controllerContext.RouteData.Values.Add("controller", viewPath);
+		}
+
+		[Test]
+		public void WillAcceptNullProperties()
+		{
+			NVelocityViewFactory factory = new NVelocityViewFactory();
+			factory = new NVelocityViewFactory(null);
+		}
+
+		[Test]
+		public void LoadValidView()
+		{
+			NVelocityView view = _factory.CreateView(_controllerContext, "view", string.Empty, null) as NVelocityView;
+			Assert.IsNotNull(view);
+			Assert.IsNotNull(view.ViewTemplate);
+		}
+
+		[Test]
+		[ExpectedException(typeof(InvalidOperationException))]
+		public void InvalidViewThrows()
+		{
+			_factory.CreateView(_controllerContext, "nonExistant", string.Empty, null);
+		}
+
+		[Test]
+		public void LoadValidViewWithMaster()
+		{
+			NVelocityView view = _factory.CreateView(_controllerContext, "view", "master", null) as NVelocityView;
+			Assert.IsNotNull(view);
+			Assert.IsNotNull(view.ViewTemplate);
+			Assert.IsNotNull(view.MasterTemplate);
+		}
+
+		[Test]
+		[ExpectedException(typeof(InvalidOperationException))]
+		public void InvalidMasterThrows()
+		{
+			_factory.CreateView(_controllerContext, "view", "nonExistant", null);
+		}
+	}
+}
