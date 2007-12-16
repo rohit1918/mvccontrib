935f5721510e8736652ae28b5a2fca5a592121f8
mvccontrib <mvccontrib@eb32e4ba-b140-0410-9c0e-b965c4cb1785>
2007-12-16 22:28:29 +0000

    CCNET build 22

---
+++ tags/22/src/MVCContrib.UnitTests/Castle/WindsorControllerFactoryTester.cs
@@ -0,0 +1,152 @@
+using System;
+using System.Web;
+using System.Web.Mvc;
+using Castle.Windsor;
+using MvcContrib.Castle;
+using NUnit.Framework;
+using NUnit.Framework.SyntaxHelpers;
+using Rhino.Mocks;
+
+namespace MvcContrib.UnitTests.Castle
+{
+	[TestFixture]
+	public class WindsorControllerFactoryTester
+	{
+		private MockRepository _mocks;
+		private IWindsorContainer _container;
+
+		[SetUp]
+		public void Setup()
+		{
+			_mocks = new MockRepository();
+
+			_container = new WindsorContainer();
+			_container.AddComponent("SimpleController", typeof(SimpleController));
+			_container.AddComponent("StubDependency", typeof(IDependency), typeof(StubDependency));
+			_container.AddComponent("DependencyController", typeof(DependencyController));
+		}
+
+		[Test]
+		public void ShouldReturnTheController()
+		{
+			IHttpContext mockContext = _mocks.DynamicMock<IHttpContext>();
+			MockApplication application = new MockApplication(_container);
+			Expect.Call(mockContext.ApplicationInstance).Return(application);
+			RequestContext context = new RequestContext(mockContext, new RouteData());
+			_mocks.ReplayAll();
+
+			IControllerFactory factory = new WindsorControllerFactory();
+
+			IController controller = factory.CreateController(context, typeof(SimpleController));
+
+			Assert.That(controller, Is.Not.Null);
+			Assert.That(controller, Is.AssignableFrom(typeof(SimpleController)));
+		}
+
+		[Test]
+		public void ShouldReturnControllerWithDependencies()
+		{
+			IHttpContext mockContext = _mocks.DynamicMock<IHttpContext>();
+			MockApplication application = new MockApplication(_container);
+			Expect.Call(mockContext.ApplicationInstance).Return(application);
+			RequestContext context = new RequestContext(mockContext, new RouteData());
+			_mocks.ReplayAll();
+
+			IControllerFactory factory = new WindsorControllerFactory();
+
+			IController controller = factory.CreateController(context, typeof(DependencyController));
+
+			Assert.That(controller, Is.Not.Null);
+			Assert.That(controller, Is.AssignableFrom(typeof(DependencyController)));
+
+			DependencyController dependencyController = (DependencyController)controller;
+			Assert.That(dependencyController._dependency, Is.Not.Null);
+			Assert.That(dependencyController._dependency, Is.AssignableFrom(typeof(StubDependency)));
+		}
+
+		[Test]
+		[ExpectedException(typeof(InvalidOperationException))]
+		public void ShouldThrowExceptionWhenContainerIsNull()
+		{
+			IHttpContext mockContext = _mocks.DynamicMock<IHttpContext>();
+			MockApplication application = new MockApplication(null);
+			Expect.Call(mockContext.ApplicationInstance).Return(application);
+			RequestContext context = new RequestContext(mockContext, new RouteData());
+			_mocks.ReplayAll();
+
+			IControllerFactory factory = new WindsorControllerFactory();
+
+			IController controller = factory.CreateController(context, typeof(SimpleController));
+		}
+
+		[Test]
+		[ExpectedException(typeof(InvalidOperationException))]
+		public void ShouldThrowExceptionWhenApplicationDoesNotImplementIContainerAccessor()
+		{
+			IHttpContext mockContext = _mocks.DynamicMock<IHttpContext>();
+			HttpApplication application = new HttpApplication();
+			Expect.Call(mockContext.ApplicationInstance).Return(application);
+			RequestContext context = new RequestContext(mockContext, new RouteData());
+			_mocks.ReplayAll();
+
+			IControllerFactory factory = new WindsorControllerFactory();
+
+			IController controller = factory.CreateController(context, typeof(SimpleController));
+		}
+
+		[Test]
+		[ExpectedException(typeof(ArgumentNullException))]
+		public void ShouldThrowExceptionWhenRequestContextIsNull()
+		{
+			IControllerFactory factory = new WindsorControllerFactory();
+
+			IController controller = factory.CreateController(null, typeof(SimpleController));
+		}
+
+		public class MockApplication : HttpApplication, IContainerAccessor
+		{
+			private readonly IWindsorContainer _container;
+
+			public MockApplication(IWindsorContainer container)
+			{
+				_container = container;
+			}
+
+			public IWindsorContainer Container
+			{
+				get { return _container; }
+			}
+		}
+
+		public class SimpleController : IController
+		{
+			public void Execute(ControllerContext controllerContext)
+			{
+				throw new NotImplementedException();
+			}
+		}
+
+		public class DependencyController : IController
+		{
+			public IDependency _dependency;
+
+			public DependencyController(IDependency dependency)
+			{
+				_dependency = dependency;
+			}
+
+			public void Execute(ControllerContext controllerContext)
+			{
+				throw new NotImplementedException();
+			}
+		}
+
+		public interface IDependency
+		{
+		}
+
+		public class StubDependency : IDependency
+		{
+		}
+	}
+}
---
+++ tags/22/src/MvcContrib.Samples.NVelocityViewFactory/Global.asax.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Reflection;
+using System.Web;
+using System.Web.Mvc;
+using Castle.Core;
+using Castle.Windsor;
+using MvcContrib.Castle;
+
+namespace MvcContrib.Samples.NVelocityViewFactory
+{
+	public class Global : HttpApplication, IContainerAccessor
+	{
+		private static WindsorContainer _container;
+
+		public static IWindsorContainer Container
+		{
+			get { return _container; }
+		}
+
+		IWindsorContainer IContainerAccessor.Container
+		{
+			get { return Container; }
+		}
+
+		protected void Application_Start(object sender, EventArgs e)
+		{
+			InitializeWindsor();
+			AddRoutes();
+		}
+
+		/// <summary>
+		/// Instantiate the container and add all Controllers that derive from 
+		/// WindsorController to the container.  Also associate the Controller 
+		/// with the WindsorContainer ControllerFactory.
+		/// </summary>
+		protected virtual void InitializeWindsor()
+		{
+			if (_container == null)
+			{
+				_container = new WindsorContainer();
+
+				// Add our singleton NVelocityViewFactory
+				_container.AddComponent("ViewFactory", typeof(IViewFactory), typeof(Castle.NVelocityViewFactory));
+
+				Type[] assemblyTypes = Assembly.GetExecutingAssembly().GetTypes();
+				
+				foreach (Type type in assemblyTypes)
+				{
+					if (typeof(IController).IsAssignableFrom(type))
+					{
+						_container.AddComponentWithLifestyle(type.Name, type, LifestyleType.Transient);
+						ControllerBuilder.Current.SetControllerFactory(type, typeof(WindsorControllerFactory));
+					}
+				}
+			}
+		}
+
+		protected virtual void AddRoutes()
+		{
+			// Set routes
+			RouteTable.Routes.Add(new Route
+			{
+				Url = "[controller].mvc/[action]/[id]",
+				Defaults = new { action = "Index", id = (string)null },
+				RouteHandler = typeof(MvcRouteHandler)
+			});
+
+			RouteTable.Routes.Add(new Route
+			{
+				Url = "[controller]/[action]/[id]",
+				Defaults = new { action = "Index", id = (string)null },
+				RouteHandler = typeof(MvcRouteHandler)
+			});
+
+			RouteTable.Routes.Add(new Route
+			{
+				Url = "Default.aspx",
+				Defaults = new { controller = "Home", action = "Index", id = (string)null },
+				RouteHandler = typeof(MvcRouteHandler)
+			});
+		}
+	}
+}
