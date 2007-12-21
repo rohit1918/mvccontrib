15d549d2a4a34de7e250d43dd6493b2be806a493
wcpierce <wcpierce@eb32e4ba-b140-0410-9c0e-b965c4cb1785>
2007-12-21 17:07:50 +0000

    Added a sample that uses the new ConventionController and examples of how to use the DeserializeAttribute

---
+++ trunk/src/MvcContrib.Samples.ConventionController/Controllers/ShipmentController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using MvcContrib;
+using MvcContrib.Attributes;
+using MvcContrib.Samples.Models;
+
+namespace MvcContrib.Samples.Controllers
+{
+	public class ShipmentController : ConventionController
+	{
+		public void Index()
+		{
+			RenderView("index");
+		}
+
+		public void New([Deserialize("shipment")] Shipment newShipment)
+		{
+			RenderView("new", newShipment);
+		}
+
+		public void Track([Deserialize("trackingNumbers")] string[] trackingNumbers)
+		{
+			List<string> validTrackingNumbers = new List<string>();
+			foreach(string trackingNumber in trackingNumbers)
+			{
+				if(!string.IsNullOrEmpty(trackingNumber))
+				{
+					validTrackingNumbers.Add(trackingNumber);
+				}
+			}
+
+			RenderView("track", validTrackingNumbers);
+		}
+
+		protected override bool OnError(string actionName, MethodInfo methodInfo, Exception exception)
+		{
+			return false;
+		}
+	}
+}
