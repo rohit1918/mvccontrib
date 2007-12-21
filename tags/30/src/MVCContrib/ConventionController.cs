a4a1cdf330cab301f7fb31dca30d99796fdbf44e
mvccontrib <mvccontrib@eb32e4ba-b140-0410-9c0e-b965c4cb1785>
2007-12-21 18:10:06 +0000

    CCNET build 30

---
+++ tags/30/src/MVCContrib/ConventionController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Mvc;
+using MvcContrib.MetaData;
+
+namespace MvcContrib
+{
+	public class ConventionController : Controller
+	{
+		protected override bool InvokeAction(string actionName)
+		{
+			if(string.IsNullOrEmpty(actionName))
+			{
+				throw new ArgumentException("actionName is required", "actionName");
+			}
+
+			IList<ActionMetaData> actions = MetaData.GetActions(actionName);
+
+			if(actions == null || actions.Count == 0)
+			{
+				return false;
+			}
+			else if(actions.Count > 1)
+			{
+				throw new InvalidOperationException(string.Format("More than one action with name '{0}' found", actionName));
+			}
+
+			try
+			{
+				InvokeActionMethod(actions[0]);
+			}
+			catch(Exception exc)
+			{
+				if(!OnError(actionName, actions[0].MethodInfo, exc))
+				{
+					throw;
+				}
+			}
+			finally
+			{
+				OnPostAction(actionName, actions[0].MethodInfo);
+			}
+
+			return true;
+		}
+
+		protected virtual void InvokeActionMethod(ActionMetaData action)
+		{
+			object[] actionParameters = new object[action.Parameters.Count];
+
+			int index = 0;
+			foreach(ActionParameterMetaData actionParameter in action.Parameters)
+			{
+				actionParameters[index++] = actionParameter.Bind(ControllerContext);
+			}
+
+			action.InvokeMethod(this, actionParameters);
+		}
+
+		private ControllerMetaData _metaData;
+
+		public ControllerMetaData MetaData
+		{
+			get
+			{
+				if(_metaData == null)
+				{
+					_metaData = ControllerDescriptor.GetMetaData(this);
+				}
+				return _metaData;
+			}
+		}
+
+		private IControllerDescriptor _controllerDescriptor;
+
+		public IControllerDescriptor ControllerDescriptor
+		{
+			get
+			{
+				if(_controllerDescriptor == null)
+				{
+					_controllerDescriptor = new CachedControllerDescriptor();
+				}
+				return _controllerDescriptor;
+			}
+			set { _controllerDescriptor = value; }
+		}
+	}
+}
