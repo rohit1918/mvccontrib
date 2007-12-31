a0fa75f3db35b0f94adf789b5e14b36daacd3879
mvccontrib <mvccontrib@eb32e4ba-b140-0410-9c0e-b965c4cb1785>
2007-12-31 04:29:18 +0000

    CCNET build 39

---
+++ tags/39/src/MVCContrib.UnitTests/ViewFactories/NVelocityHtmlHelperTester.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Web;
+using System.Web.Mvc;
+using MvcContrib.Castle;
+using NUnit.Framework;
+using Rhino.Mocks;
+
+namespace MVCContrib.UnitTests.ViewFactories
+{
+	[TestFixture, Category("NVelocityViewEngine")]
+	public class NVelocityHtmlHelperTester
+	{
+		private MockRepository _mocks;
+		private NVelocityHtmlHelper _htmlHelper;
+
+		[SetUp]
+		public void SetUp()
+		{
+			_mocks = new MockRepository();
+			IHttpContext httpContext = _mocks.DynamicMock<IHttpContext>();
+			IHttpResponse httpResponse = _mocks.DynamicMock<IHttpResponse>();
+			IHttpSessionState httpSessionState = _mocks.DynamicMock<IHttpSessionState>();
+			IHttpServerUtility httpServer = _mocks.DynamicMock<IHttpServerUtility>();
+			SetupResult.For(httpContext.Session).Return(httpSessionState);
+			SetupResult.For(httpContext.Response).Return(httpResponse);
+			SetupResult.For(httpContext.Server).Return(httpServer);
+			SetupResult.For(httpServer.HtmlEncode(null)).IgnoreArguments().Return(string.Empty);
+			RequestContext requestContext = new RequestContext(httpContext, new RouteData());
+			IController controller = _mocks.DynamicMock<IController>();
+			ControllerContext controllerContext = new ControllerContext(requestContext, controller);
+			_mocks.ReplayAll();
+			ViewContext viewContext =
+				new ViewContext(controllerContext, new Hashtable(), new TempDataDictionary(controllerContext.HttpContext));
+
+			_htmlHelper = new NVelocityHtmlHelper(viewContext);
+		}
+
+		[Test]
+		public void Submit_Defaults_To_No_HtmlName()
+		{
+			Assert.AreEqual(_htmlHelper.SubmitButton(string.Empty, "test"), _htmlHelper.Submit("test"));
+		}
+
+		[Test]
+		public void Submit_Uses_Id_For_HtmlName()
+		{
+			Hashtable htmlAttributes = new Hashtable();
+			htmlAttributes["id"] = "id";
+
+			Assert.AreEqual(_htmlHelper.SubmitButton("id", "test"), _htmlHelper.Submit("test", htmlAttributes));
+		}
+
+		[Test]
+		public void TextBox_Passes_Through_Attributes()
+		{
+			Hashtable htmlAttributes = new Hashtable();
+			htmlAttributes["attr"] = "value";
+
+			Assert.AreEqual(_htmlHelper.TextBox("htmlName", string.Empty, new { attr = "value" }), _htmlHelper.TextBox("htmlName", htmlAttributes));
+		}
+
+		[Test]
+		public void TextBox_Passes_Through_Attributes_With_Value()
+		{
+			Hashtable htmlAttributes = new Hashtable();
+			htmlAttributes["attr"] = "value";
+
+			Assert.AreEqual(_htmlHelper.TextBox("htmlName", "value", new { attr = "value" }), _htmlHelper.TextBox("htmlName", "value", htmlAttributes));
+		}
+
+		[Test]
+		public void Mailto_Uses_Subject_Body_Cc_And_Bcc_Attributes()
+		{
+			Hashtable htmlAttributes = new Hashtable();
+			htmlAttributes["subject"] = "subject";
+			htmlAttributes["body"] = "body";
+			htmlAttributes["cc"] = "cc";
+
+			Assert.AreEqual(_htmlHelper.MailTo("emailAddress", "linkText", "subject", string.Empty, "cc", "body"), _htmlHelper.MailTo("emailAddress", "linkText", htmlAttributes));
+		}
+	}
+}
