10ecb13548193d22299309174f0384930a0863ed
mvccontrib <mvccontrib@eb32e4ba-b140-0410-9c0e-b965c4cb1785>
2007-12-31 21:45:38 +0000

    CCNET build 40

---
+++ tags/40/src/MVCContrib.UnitTests/NHamlViewEngine/NHamlConfigurationTester.cs
@@ -0,0 +1,18 @@
+﻿using MvcContrib.NHamlViewEngine.Configuration;
+using NUnit.Framework;
+
+namespace MvcContrib.UnitTests.NHamlViewEngine
+{
+	[TestFixture]
+	public class ConfigurationTester
+	{
+		[Test]
+		public void Can_Read_Production_Setting_From_AppSettings()
+		{
+			NHamlViewEngineSection section = NHamlViewEngineSection.Read();
+
+			Assert.IsNotNull(section);
+			Assert.IsFalse(section.Production);
+		}
+	}
+}
