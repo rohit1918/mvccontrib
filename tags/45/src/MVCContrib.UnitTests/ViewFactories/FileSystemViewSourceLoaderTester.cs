11a3a7f7881aeaa2d108673f1628681e51af3ed4
mvccontrib <mvccontrib@eb32e4ba-b140-0410-9c0e-b965c4cb1785>
2008-01-08 02:29:41 +0000

    CCNET build 45

---
+++ tags/45/src/MVCContrib.UnitTests/ViewFactories/FileSystemViewSourceLoaderTester.cs
@@ -0,0 +1,31 @@
+﻿using System.Web.UI.WebControls;
+using MvcContrib.ViewFactories;
+using NUnit.Framework;
+
+namespace MVCContrib.UnitTests.ViewFactories
+{
+	[TestFixture, Category("ViewFactories")]
+	public class FileSystemViewSourceLoaderTester
+	{
+		[Test]
+		public void HasView_ReturnsFalse_For_Non_Existing_Views()
+		{
+			FileSystemViewSourceLoader viewSourceLoader = new FileSystemViewSourceLoader("C:\\");
+
+			Assert.IsFalse(viewSourceLoader.HasView("D:\\MyLovelyView"));
+		}
+
+		[Test]
+		public void GetViewSource_ReturnsNull_For_Non_Existing_Views()
+		{
+			FileSystemViewSourceLoader viewSourceLoader = new FileSystemViewSourceLoader();
+
+			if (viewSourceLoader.ViewRootDirectory == null)
+			{
+				viewSourceLoader.ViewRootDirectory = "C:\\";
+			}
+
+			Assert.IsNull(viewSourceLoader.GetViewSource("D:\\MyLovelyView"));
+		}
+	}
+}
