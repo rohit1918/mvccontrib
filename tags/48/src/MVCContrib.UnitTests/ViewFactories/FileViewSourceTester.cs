46b91ed5574ebf8815014bd6faa772a6ff187098
mvccontrib <mvccontrib@eb32e4ba-b140-0410-9c0e-b965c4cb1785>
2008-01-08 04:59:49 +0000

    CCNET build 48

---
+++ tags/48/src/MVCContrib.UnitTests/ViewFactories/FileViewSourceTester.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using MvcContrib.ViewFactories;
+using NUnit.Framework;
+
+namespace MVCContrib.UnitTests.ViewFactories
+{
+	[TestFixture, Category("ViewFactories")]
+	public class FileViewSourceTester
+	{
+		[Test]
+		[ExpectedException(typeof(ArgumentNullException))]
+		public void FileViewSource_DependsOn_FileInfo()
+		{
+			new FileViewSource(null);
+		}
+
+		[Test]
+		public void Can_Open_Existing_File()
+		{
+			FileInfo fileInfo = new FileInfo(Assembly.GetExecutingAssembly().Location);
+			FileViewSource viewSource = new FileViewSource(fileInfo);
+
+			Assert.AreEqual(fileInfo.LastWriteTimeUtc.Ticks, viewSource.LastModified);
+
+			using(Stream viewSourceStream = viewSource.OpenViewStream())
+			{
+				Assert.IsNotNull(viewSourceStream);
+			}
+		}
+	}
+}
---
+++ tags/48/src/MvcContrib.Samples.NVelocityViewFactory/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Web.Mvc;
+using MvcContrib.Samples.NVelocityViewFactory.Models;
+
+namespace MvcContrib.Samples.NVelocityViewFactory.Controllers
+{
+	public class HomeController : Controller
+	{
+		[ControllerAction]
+		public void Index()
+		{
+			RenderView("Index", "Site");
+		}
+
+		[ControllerAction]
+		public void Contact()
+		{
+			CompanyInfo companyInfo = new CompanyInfo();
+			companyInfo.CompanyName = "Your company name here";
+			companyInfo.AddressLine1 = "Company address Line 1";
+			companyInfo.AddressLine2 = "Company address Line 2";
+			companyInfo.City = "City";
+			companyInfo.State = "State";
+			companyInfo.Zip = "00000";
+			companyInfo.Email = "email@yourcompany.com";
+			
+			RenderView("Contact", "Site", companyInfo);
+		}
+
+		[ControllerAction]
+		public void About()
+		{
+			ViewData["now"] = DateTime.Now;
+
+			RenderView("About", "Site");
+		}
+	}
+}
