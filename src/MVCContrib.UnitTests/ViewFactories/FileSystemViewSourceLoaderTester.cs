90ef6c43dfcd7c27ca11776c62478de653251aff
wcpierce <wcpierce@eb32e4ba-b140-0410-9c0e-b965c4cb1785>
2008-01-08 04:57:20 +0000

    Issue#: 785
    Ported Brail ViewEngine from MonoRail project.  This is the first step in helping teams interested in migrating existing investments in MonoRail to ASP.Net MVC.  The code was intentionally modified as little as possible to help with the porting of future bug fixes.  Two major omissions from MonoRail are ViewComponents and JavaScript partials.  Extension methods are not yet supported.

--- src/MVCContrib.UnitTests/ViewFactories/FileSystemViewSourceLoaderTester.cs
+++ src/MVCContrib.UnitTests/ViewFactories/FileSystemViewSourceLoaderTester.cs
@@ -1,4 +1,5 @@
-﻿using System.Web.UI.WebControls;
+﻿using System.IO;
+using System.Web.UI.WebControls;
 using MvcContrib.ViewFactories;
 using NUnit.Framework;
 
@@ -26,6 +27,35 @@ namespace MVCContrib.UnitTests.ViewFactories
 			}
 
 			Assert.IsNull(viewSourceLoader.GetViewSource("D:\\MyLovelyView"));
+		}
+
+		[Test]
+		public void ListViews_Returns_Empty_Array_For_Invalid_Directory()
+		{
+			FileSystemViewSourceLoader viewSourceLoader = new FileSystemViewSourceLoader();
+
+			if (viewSourceLoader.ViewRootDirectory == null)
+			{
+				viewSourceLoader.ViewRootDirectory = "C:\\";
+			}
+
+			string[] views = viewSourceLoader.ListViews("MyViewDir");
+
+			Assert.IsNotNull(views);
+			Assert.AreEqual(0, views.Length);
+		}
+
+		[Test]
+		public void Can_Add_And_Remove_Listeners()
+		{
+			FileSystemViewSourceLoader viewSourceLoader = new FileSystemViewSourceLoader("C:\\");
+
+			FileSystemEventHandler handler = delegate { };
+
+			viewSourceLoader.ViewRootDirectoryChanged += handler;
+			viewSourceLoader.ViewRootDirectoryChanged -= handler;
+			viewSourceLoader.ViewRootDirectoryChanged += handler;
+			viewSourceLoader.ViewRootDirectoryChanged -= handler;
 		}
 	}
 }
