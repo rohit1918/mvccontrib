17cfa793be5a97af58cad08ab0caf01b6a7ec55c
mvccontrib <mvccontrib@eb32e4ba-b140-0410-9c0e-b965c4cb1785>
2008-01-13 04:13:07 +0000

    CCNET build 55

---
+++ tags/55/src/MvcContrib.Samples.IoC/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Web;
+using System.Web.Mvc;
+using StructureMap;
+
+namespace MvcContrib.Samples.IoC.Controllers
+{
+    [PluginFamily("Default")]
+    [Pluggable("Default")]
+    public class HomeController : Controller
+    {
+        [ControllerAction]
+        public void Index()
+        {
+            RenderView("Index");
+        }
+
+        [ControllerAction]
+        public void About()
+        {
+            RenderView("About");
+        }
+    }
+}
