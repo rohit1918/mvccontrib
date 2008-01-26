ecc133e732ad290ded01073670bf33edf774d05e
eric.hexter <eric.hexter@eb32e4ba-b140-0410-9c0e-b965c4cb1785>
2008-01-26 06:58:30 +0000

    reverted build.  ncover is blowing up on cruise build.

--- src/MvcContrib.Samples.IoC/Global.asax.cs
+++ src/MvcContrib.Samples.IoC/Global.asax.cs
@@ -6,6 +6,10 @@ using MvcContrib.ControllerFactories;
 using MvcContrib.Samples.IoC.Controllers;
 using MvcContrib.Services;
 using MvcContrib.UnitTests.ControllerFactories;
+using MVCContrib.UnitTests.IoC;
+using Spring.Core.IO;
+using Spring.Objects.Factory;
+using Spring.Objects.Factory.Xml;
 using StructureMap;
 
 namespace MvcContrib.Samples.IoC
@@ -22,9 +26,13 @@ namespace MvcContrib.Samples.IoC
 
         private void ConfigureIoC()
         {
-            StructureMapConfiguration.UseDefaultStructureMapConfigFile = false;
-            StructureMapConfiguration.BuildInstancesOf<HomeController>().TheDefaultIsConcreteType<HomeController>();
-            DependencyResolver.InitializeWith(new StructureMapDependencyResolver());
+            //StructureMapConfiguration.UseDefaultStructureMapConfigFile = false;
+            //StructureMapConfiguration.BuildInstancesOf<HomeController>().TheDefaultIsConcreteType<HomeController>();
+
+            IResource input = new FileSystemResource(Server.MapPath("objects.xml"));
+            IObjectFactory factory = new XmlObjectFactory(input);
+            DependencyResolver.InitializeWith(new SpringDependencyResolver(factory));
+            
             ControllerBuilder.Current.SetDefaultControllerFactory(typeof(IoCControllerFactory));
         }
 
