f00a1674ba96272b280b750d0ef7a726a9f17598
mvccontrib <mvccontrib@eb32e4ba-b140-0410-9c0e-b965c4cb1785>
2008-01-28 16:27:33 +0000

    CCNET build 57

---
+++ trunk/src/MvcContrib.Samples.ConventionController/Controllers/ShipmentController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using MvcContrib;
+using MvcContrib.Attributes;
+using MvcContrib.Samples.Models;
+
+namespace MvcContrib.Samples.Controllers
+{
+	public class ShipmentController : ConventionController
+	{
+		public void Index()
+		{
+			RenderView("index");
+		}
+
+		public void New([Deserialize("shipment")] Shipment newShipment)
+		{
+			RenderView("new", newShipment);
+		}
+
+		public void Track([Deserialize("trackingNumbers")] string[] trackingNumbers)
+		{
+			List<string> validTrackingNumbers = new List<string>();
+			foreach(string trackingNumber in trackingNumbers)
+			{
+				if(!string.IsNullOrEmpty(trackingNumber))
+				{
+					validTrackingNumbers.Add(trackingNumber);
+				}
+			}
+
+			RenderView("track", validTrackingNumbers);
+		}
+
+		[Rescue("Error")]
+		public void ToTheRescue()
+		{
+			throw new InvalidOperationException();
+		}
+
+		[HiddenAction]
+		public void Hidden()
+		{
+			Response.Write("This action cannot be called.");
+		}
+
+		[DefaultAction]
+		public void DefaultAction()
+		{
+			Response.Write(string.Format("You tried to access action '{0}' but it does not exit.", SelectedAction));
+		}
+
+		protected override bool OnError(string actionName, MethodInfo methodInfo, Exception exception)
+		{
+			return false;
+		}
+	}
+}
