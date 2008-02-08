fa8762f1f930e0d7bbd081e6e03976f059b13709
mvccontrib <mvccontrib@eb32e4ba-b140-0410-9c0e-b965c4cb1785>
2008-02-08 17:46:52 +0000

    CCNET build 59

---
+++ trunk/src/MvcContrib.NHamlViewEngine/NHamlViewFactory.cs
@@ -0,0 +1,175 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data.Linq;
+using System.Diagnostics.CodeAnalysis;
+using System.IO;
+using System.Reflection;
+using System.Security.Permissions;
+using System.Web;
+using System.Web.Mvc;
+using MvcContrib.NHamlViewEngine;
+using MvcContrib.NHamlViewEngine.Configuration;
+using MvcContrib.NHamlViewEngine.Utilities;
+
+namespace MvcContrib.ViewFactories
+{
+	[AspNetHostingPermission(SecurityAction.InheritanceDemand, Level = AspNetHostingPermissionLevel.Minimal)]
+	[AspNetHostingPermission(SecurityAction.LinkDemand, Level = AspNetHostingPermissionLevel.Minimal)]
+	public class NHamlViewFactory : IViewFactory
+	{
+		private static readonly Dictionary<string, CompiledView> _viewCache = new Dictionary<string, CompiledView>();
+
+		private static readonly TemplateCompiler _templateCompiler = new TemplateCompiler();
+
+		private static bool _production;
+
+		private readonly IViewSourceLoader _viewSourceLoader;
+
+		[SuppressMessage("Microsoft.Performance", "CA1810")]
+		static NHamlViewFactory()
+		{
+			_templateCompiler.AddUsing("System.Web");
+			_templateCompiler.AddUsing("System.Web.Mvc");
+			_templateCompiler.AddUsing("MvcContrib.NHamlViewEngine");
+
+			_templateCompiler.ViewBaseType = typeof(NHamlView<>);
+
+			_templateCompiler.AddReference(typeof(IView).Assembly.Location);
+			_templateCompiler.AddReference(typeof(DataContext).Assembly.Location);
+			_templateCompiler.AddReference(typeof(TextInputExtensions).Assembly.Location);
+
+			LoadConfiguration();
+		}
+
+		public NHamlViewFactory()
+			: this(new FileSystemViewSourceLoader())
+		{
+		}
+
+		public NHamlViewFactory(IViewSourceLoader viewSourceLoader)
+		{
+			Invariant.ArgumentNotNull(viewSourceLoader, "viewSourceLoader");
+
+			_viewSourceLoader = viewSourceLoader;
+		}
+
+		private static void LoadConfiguration()
+		{
+			NHamlViewEngineSection section = NHamlViewEngineSection.Read();
+
+			if(section != null)
+			{
+				_production = section.Production;
+
+				foreach(AssemblyConfigurationElement cfgAsm in section.Views.Assemblies)
+				{
+					_templateCompiler.AddReference(Assembly.Load(cfgAsm.Name).Location);
+				}
+
+				foreach(NamespaceConfigurationElement cfgNs in section.Views.Namespaces)
+				{
+					_templateCompiler.AddUsing(cfgNs.Name);
+				}
+			}
+		}
+
+		public IView CreateView(ControllerContext controllerContext, string viewName,
+		                        string masterName, object viewData)
+		{
+			string controller = (string)controllerContext.RouteData.Values["controller"];
+			string viewKey = controller + "/" + viewName;
+
+			CompiledView compiledView;
+
+			if(!_viewCache.TryGetValue(viewKey, out compiledView))
+			{
+				lock(_viewCache)
+				{
+					if(!_viewCache.TryGetValue(viewKey, out compiledView))
+					{
+						string viewPath = viewKey;
+
+						if(!Path.HasExtension(viewPath))
+						{
+							viewPath = string.Concat(viewPath, ".haml");
+						}
+
+						IViewSource viewSource = _viewSourceLoader.GetViewSource(viewPath);
+
+						Invariant.IsNotNull(viewSource);
+
+						IViewSource layoutSource = FindLayout("Shared", masterName, controller);
+
+						string layoutPath = null;
+
+						if(layoutSource != null)
+						{
+							layoutPath = layoutSource.FullName;
+						}
+
+						compiledView = new CompiledView(_templateCompiler, viewSource.FullName, layoutPath, viewData);
+
+						_viewCache.Add(viewKey, compiledView);
+					}
+				}
+			}
+
+			if(!_production)
+			{
+				compiledView.RecompileIfNecessary(viewData);
+			}
+
+			INHamlView view = compiledView.CreateView();
+
+			if(ViewDataIsDictionary(viewData))
+			{
+				viewData = new ViewData(viewData);
+			}
+
+			view.SetViewData(viewData);
+
+			return view;
+		}
+
+		public static bool ViewDataIsDictionary(object viewData)
+		{
+			return (viewData != null)
+			       && (typeof(IDictionary).IsAssignableFrom(viewData.GetType()));
+		}
+
+		protected virtual IViewSource FindLayout(string mastersFolder, string masterName, string controller)
+		{
+			string masterPath = mastersFolder + "\\" + masterName + ".haml";
+
+			if(_viewSourceLoader.HasView(masterPath))
+			{
+				return _viewSourceLoader.GetViewSource(masterPath);
+			}
+
+			masterPath = mastersFolder + "\\" + controller + ".haml";
+
+			if(_viewSourceLoader.HasView(masterPath))
+			{
+				return _viewSourceLoader.GetViewSource(masterPath);
+			}
+
+			masterPath = mastersFolder + "\\application.haml";
+
+			if(_viewSourceLoader.HasView(masterPath))
+			{
+				return _viewSourceLoader.GetViewSource(masterPath);
+			}
+
+			return null;
+		}
+
+		public static void ClearViewCache()
+		{
+			lock(_viewCache)
+			{
+				_viewCache.Clear();
+			}
+		}
+	}
+}
---
+++ trunk/src/MvcContrib.ObjectBuilder/DependencyContainer/DependencyContainer.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Practices.CompositeWeb.ObjectBuilder;
+using Microsoft.Practices.ObjectBuilder;
+
+namespace MvcContrib.ObjectBuilder
+{
+	public class DependencyContainer : IDisposable, IDependencyContainer
+	{
+		private IBuilder<BuilderStage> builder;
+		private LifetimeContainer lifetime;
+		private Locator locator;
+
+		// Lifetime
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="DependencyContainer"/> class
+		/// that supports attribute-based reflection.
+		/// </summary>
+		public DependencyContainer()
+			: this(true)
+		{
+		}
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="DependencyContainer"/> class.
+		/// </summary>
+		/// <param name="enableReflection">Set to <c>true</c> to enable support for
+		/// attribute-based reflection</param>
+		public DependencyContainer(bool enableReflection)
+			: this(new ContainerXmlConfig(enableReflection))
+		{
+		}
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="DependencyContainer"/> class.
+		/// </summary>
+		/// <param name="xml">The XML configuration for the container</param>
+		public DependencyContainer(string xml)
+			: this(new ContainerXmlConfig(xml))
+		{
+		}
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="DependencyContainer"/> class.
+		/// </summary>
+		/// <param name="configurator">The container configurator.</param>
+		public DependencyContainer(IBuilderConfigurator<BuilderStage> configurator)
+		{
+			builder = new WCSFBuilderBase<BuilderStage>(configurator);
+			locator = new Locator();
+			lifetime = new LifetimeContainer();
+			locator.Add(typeof(ILifetimeContainer), lifetime);
+		}
+
+		/// <summary>
+		/// Performs application-defined tasks associated with freeing, releasing,
+		/// or resetting unmanaged resources.
+		/// </summary>
+		public void Dispose()
+		{
+			if(lifetime != null)
+			{
+				List<object> items = new List<object>();
+				items.AddRange(lifetime);
+
+				foreach(object item in items)
+					builder.TearDown(locator, item);
+
+				lifetime.Dispose();
+				lifetime = null;
+				locator = null;
+				builder = null;
+			}
+		}
+
+		// Methods
+
+		public object Inject(object @object)
+		{
+			if(@object != null)
+			{
+				return Inject(@object, @object.GetType());
+			}
+			else
+			{
+				return null;
+			}
+		}
+
+		public object Inject<TToBuild>(object @object)
+		{
+			if(@object != null)
+			{
+				return Inject(@object, typeof(TToBuild));
+			}
+			else
+			{
+				return null;
+			}
+		}
+
+		public object Inject(object @object, Type itemType)
+		{
+			if(@object != null)
+			{
+				string temporaryId = Guid.NewGuid().ToString();
+				PolicyList policies = new PolicyList(new PolicyList[0]);
+				policies.Set<ISingletonPolicy>(new SingletonPolicy(false), itemType, temporaryId);
+				policies.Set<ICreationPolicy>(new DefaultCreationPolicy(), itemType, temporaryId);
+				policies.Set<IPropertySetterPolicy>(new PropertySetterPolicy(), itemType, temporaryId);
+				return builder.BuildUp(locator, itemType, temporaryId, @object, new PolicyList[] {policies});
+			}
+			else
+			{
+				return null;
+			}
+		}
+
+		/// <summary>
+		/// Finds all the singletons in the container that implement the given type.
+		/// </summary>
+		/// <returns>An enumeration of the matching items</returns>
+		public IEnumerable<T> FindSingletons<T>()
+		{
+			foreach(object obj in lifetime)
+				if(obj is T)
+					yield return (T)obj;
+		}
+
+		/// <summary>
+		/// Gets an object of the given type from the container.
+		/// </summary>
+		/// <returns>The object</returns>
+		public TBuild Get<TBuild>()
+		{
+			return builder.BuildUp<TBuild>(locator, null, null);
+		}
+
+		/// <summary>
+		/// Gets an object of the given type from the container.
+		/// </summary>
+		/// <returns>The object</returns>
+		public object Get(Type tBuild)
+		{
+			return builder.BuildUp(locator, tBuild, null, null);
+		}
+
+		/// <summary>
+		/// Registers a singleton instance in the container.
+		/// </summary>
+		/// <typeparam name="TBuild">The type of the singleton</typeparam>
+		/// <param name="item">The item instance to be registered as the singleton</param>
+		public void RegisterInstance<TBuild>(TBuild item)
+		{
+			RegisterSingleton<TBuild>();
+			builder.BuildUp<TBuild>(locator, null, item);
+			Inject<TBuild>(item);
+		}
+
+		/// <summary>
+		/// Registers the given type as a singleton in the container.
+		/// </summary>
+		/// <typeparam name="TBuild">The type to be made a singleton</typeparam>
+		public void RegisterSingleton<TBuild>()
+		{
+			builder.Policies.Set<ISingletonPolicy>(new SingletonPolicy(true), typeof(TBuild), null);
+		}
+
+		/// <summary>
+		/// Registers a type mapping in the container.
+		/// </summary>
+		/// <typeparam name="TRequested">The type that is requested by the user</typeparam>
+		/// <typeparam name="TToBuild">The type to be built instead</typeparam>
+		public void RegisterTypeMapping<TRequested, TToBuild>()
+		{
+			RegisterTypeMapping(typeof(TRequested), typeof(TToBuild));
+		}
+
+		/// <summary>
+		/// Registers a type mapping in the container.
+		/// </summary>
+		/// <typeparam name="TRequested">The type that is requested by the user</typeparam>
+		/// <typeparam name="TToBuild">The type to be built instead</typeparam>
+		public void RegisterTypeMapping(Type requestedType, Type typeToBuild)
+		{
+			builder.Policies.Set<ITypeMappingPolicy>(new TypeMappingPolicy(typeToBuild, null), requestedType, null);
+		}
+	}
+}
