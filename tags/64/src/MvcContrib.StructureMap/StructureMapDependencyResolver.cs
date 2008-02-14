d22870268854582d9da66743932438b049d19282
mvccontrib <mvccontrib@eb32e4ba-b140-0410-9c0e-b965c4cb1785>
2008-02-14 18:25:14 +0000

    CCNET build 64

---
+++ tags/64/src/MvcContrib.StructureMap/StructureMapDependencyResolver.cs
@@ -0,0 +1,31 @@
+namespace MvcContrib.UnitTests.ControllerFactories
+{
+	using System;
+	using MvcContrib.Interfaces;
+	using StructureMap;
+
+	public class StructureMapDependencyResolver : IDependencyResolver
+	{
+		public TType GetImplementationOf<TType>()
+		{
+			return (TType)GetImplementationOf(typeof(TType));
+		}
+
+		public Interface GetImplementationOf<Interface>(Type type)
+		{
+			return (Interface)GetImplementationOf(type);
+		}
+
+		public object GetImplementationOf(Type type)
+		{
+			try
+			{
+				return ObjectFactory.GetInstance(type);
+			}
+			catch(StructureMapException)
+			{
+				return null;
+			}
+		}
+	}
+}
