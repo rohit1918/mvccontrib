17f3feedbbea805ccf57728c4b60636b9b0f04d3
mvccontrib <mvccontrib@eb32e4ba-b140-0410-9c0e-b965c4cb1785>
2008-02-15 03:24:30 +0000

    CCNET build 66

---
+++ tags/66/src/MvcContrib.ObjectBuilder/ObjectFactoryDependencyResolver.cs
@@ -0,0 +1,38 @@
+using System;
+using MvcContrib.Interfaces;
+using MvcContrib.ObjectBuilder;
+
+namespace MVCContrib.UnitTests.IoC
+{
+	public class ObjectFactoryDependencyResolver : IDependencyResolver
+	{
+		private readonly IDependencyContainer _container;
+
+		public ObjectFactoryDependencyResolver(IDependencyContainer container)
+		{
+			_container = container;
+		}
+
+		public Interface GetImplementationOf<Interface>()
+		{
+			return (Interface)GetImplementationOf(typeof(Interface));
+		}
+
+		public Interface GetImplementationOf<Interface>(Type type)
+		{
+			return (Interface)GetImplementationOf(type);
+		}
+
+		public object GetImplementationOf(Type type)
+		{
+			try
+			{
+				return _container.Get(type);
+			}
+			catch(ArgumentException)
+			{
+				return null;
+			}
+		}
+	}
+}
