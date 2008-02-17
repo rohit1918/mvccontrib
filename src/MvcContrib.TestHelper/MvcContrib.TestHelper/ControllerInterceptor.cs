29268fb104a9c4bebb1999ed5294d65a7dc3a7ca
eric.hexter <eric.hexter@eb32e4ba-b140-0410-9c0e-b965c4cb1785>
2008-02-17 01:31:34 +0000

    Added the TestHelper ( Test Framework) Contributed by Will Shaver. Samples to come in next commit.

---
+++ src/MvcContrib.TestHelper/MvcContrib.TestHelper/ControllerInterceptor.cs
@@ -0,0 +1,82 @@
+﻿using System.Reflection;
+using System.Web.Mvc;
+using Castle.Core.Interceptor;
+
+namespace MvcTestingFramework
+{
+    public partial class TestControllerBuilder //remember c++ "friend" classes?
+    {
+        #region Nested type: ControllerInterceptor
+
+        private class ControllerInterceptor : IInterceptor
+        {
+            private TestControllerBuilder _parentHandler;
+
+            public ControllerInterceptor(TestControllerBuilder parentHandler)
+            {
+                _parentHandler = parentHandler;
+            }
+
+            #region IInterceptor Members
+
+            public void Intercept(IInvocation invocation)
+            {
+                if(invocation.Method.Name == "RenderView" && invocation.Arguments.Length == 3)
+                {
+                    string viewName = (string)invocation.Arguments[0];
+                    string masterName = (string)invocation.Arguments[1];
+                    object viewData = (object)invocation.Arguments[2];
+                    _parentHandler.RenderViewData = new RenderViewData
+                    {
+                        ViewName = viewName,
+                        MasterName = masterName,
+                        ViewData = viewData
+                    }
+                    ;
+                    return;
+                }
+                if(invocation.Method.Name == "RedirectToAction" && invocation.Arguments.Length == 1)
+                {
+                    HandleRedirectToAction(invocation.Arguments[0]);
+                    return;
+                }
+                /*
+                if(invocation.Method.GetCustomAttributes(typeof(ControllerActionAttribute), false).Length > 0)
+                {
+                    //Could add code here to build the RouteData of the controller (by assigning _parentHandler.RouteData)
+                    //based on the method call and arguments in combination with the defined route table
+                }
+                */
+                invocation.Proceed();
+            }
+
+            #endregion
+
+            protected void HandleRedirectToAction(object values)
+            {
+                //crazy reflection stuff because RedirectData and RedirectDataWithController are marked as internal. (Rage!)
+                PropertyInfo[] props = values.GetType().GetProperties();
+                string actionName = "", controllerName = "";
+                foreach(PropertyInfo p in props)
+                {
+                    if(p.Name == "Action")
+                    {
+                        actionName = (string)p.GetValue(values, null);
+                    }
+                    else if(p.Name == "Controller")
+                    {
+                        controllerName = (string)p.GetValue(values, null);
+                    }
+                }
+                _parentHandler.RedirectToActionData = new RedirectToActionData
+                {
+                    ActionName = actionName,
+                    ControllerName = controllerName
+                }
+                ;
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ src/MvcContrib.TestHelper/MvcContrib.TestHelper/RenderViewData.cs
@@ -0,0 +1,23 @@
+﻿namespace MvcTestingFramework
+{
+    public class RenderViewData
+    {
+        public string ViewName
+        {
+            get;
+            set;
+        }
+
+        public string MasterName
+        {
+            get;
+            set;
+        }
+
+        public object ViewData
+        {
+            get;
+            set;
+        }
+    }
+}
