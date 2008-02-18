55a8d796e5013b1d2fdf571cb7c3f9599faed6ef
jhandel@ktomics.net <jhandel@ktomics.net@eb32e4ba-b140-0410-9c0e-b965c4cb1785>
2008-02-18 20:23:59 +0000

    Merging in Tag Generation and FormHelper functionality from branch TagHelper. * New Namespace MvcContrib.UI * New Tag Generation classes based on new Element and ScriptableElement base classes * New MvcContrib.UI.Tags namespace with many tags for Forms and other stuff * New MvcContrib.UI.Html with FormsHelper functionality * New Tests (100% coverage on all new functionality)

---
+++ trunk/src/MvcContrib.Samples.FormHelper/Global.asax.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Security.Policy;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using System.Web.SessionState;
+using System.Xml.Linq;
+
+namespace MvcContrib.Samples.FormHelper
+{
+	public class Global : System.Web.HttpApplication
+	{
+
+		protected void Application_Start(object sender, EventArgs e)
+		{
+			RouteTable.Routes.Add(new Route
+			{
+				Url = "[controller].mvc/[action]",
+				Defaults = new { action = "Index", id = (string)null },
+				RouteHandler = typeof(MvcRouteHandler)
+			});
+
+			RouteTable.Routes.Add(new Route
+			{
+				Url = "Default.aspx",
+				Defaults = new { controller = "Home", action = "Index", id = (string)null },
+				RouteHandler = typeof(MvcRouteHandler)
+			});
+		}
+
+	}
+}
