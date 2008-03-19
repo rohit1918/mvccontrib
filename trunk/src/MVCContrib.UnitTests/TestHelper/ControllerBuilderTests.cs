201e5b50dc7824842efe847e4aa43486b2c00986
Jeremy.Skinner <Jeremy.Skinner@eb32e4ba-b140-0410-9c0e-b965c4cb1785>
2008-03-19 18:43:29 +0000

    Patch #1003 - kbaley Patch includes: - the ability to use TestControllerBuilder with controllers that have constructor arguments - associated unit test

--- trunk/src/MVCContrib.UnitTests/TestHelper/ControllerBuilderTests.cs
+++ trunk/src/MVCContrib.UnitTests/TestHelper/ControllerBuilderTests.cs
@@ -1,12 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using System.Web.Routing;
 using NUnit.Framework;
 using MvcContrib.TestHelper;
+using NUnit.Framework.SyntaxHelpers;
+using Rhino.Mocks;
 
 
 //Note: these tests confirm that the TestControllerBuilder works properly
@@ -14,97 +11,118 @@ using MvcContrib.TestHelper;
 //look in the \src\Samples\MvcContrib.TestHelper solution
 namespace MvcContrib.TestHelper.Test
 {
-    [TestFixture]
-    public class ControllerBuilderTests
-    {
-        [Test]
-        public void CanCreateSpecificController()
-        {
-            TestControllerBuilder builder = new TestControllerBuilder();
-            object c = builder.CreateController<TestHelperController>();
-            Assert.IsInstanceOfType(typeof(TestHelperController), c);
-        }
+	[TestFixture]
+	public class ControllerBuilderTests
+	{
+		[Test]
+		public void CanCreateSpecificController()
+		{
+			TestControllerBuilder builder = new TestControllerBuilder();
+			object c = builder.CreateController<TestHelperController>();
+			Assert.IsInstanceOfType(typeof(TestHelperController), c);
+		}
 
-        [Test]
-        public void CanSpecifyFormVariables()
-        {
-            TestControllerBuilder builder = new TestControllerBuilder();
-            builder.Form["Variable"] = "Value";
-            Controller c = builder.CreateController<TestHelperController>();
-            Assert.AreEqual("Value", c.HttpContext.Request.Form["Variable"]);
-        }
+		[Test]
+		public void CanSpecifyFormVariables()
+		{
+			TestControllerBuilder builder = new TestControllerBuilder();
+			builder.Form["Variable"] = "Value";
+			Controller c = builder.CreateController<TestHelperController>();
+			Assert.AreEqual("Value", c.HttpContext.Request.Form["Variable"]);
+		}
 
-        [Test]
-        public void CanSpecifyRouteData()
-        {
-            TestControllerBuilder builder = new TestControllerBuilder();
-            RouteData rd = new RouteData();
-            rd.Values["Variable"] = "Value";
-            builder.RouteData = rd;
+		[Test]
+		public void CanSpecifyRouteData()
+		{
+			TestControllerBuilder builder = new TestControllerBuilder();
+			RouteData rd = new RouteData();
+			rd.Values["Variable"] = "Value";
+			builder.RouteData = rd;
 
-            TestHelperController testController = builder.CreateController<TestHelperController>();
-            Assert.AreEqual("Value", testController.RouteData.Values["Variable"]);
-        }
+			TestHelperController testController = builder.CreateController<TestHelperController>();
+			Assert.AreEqual("Value", testController.RouteData.Values["Variable"]);
+		}
 
-        [Test]
-        public void CanSpecifyQueryString()
-        {
-            TestControllerBuilder handler = new TestControllerBuilder();
-            handler.QueryString["Variable"] = "Value";
-            TestHelperController testController = handler.CreateController<TestHelperController>();
-            Assert.AreEqual("Value", testController.Request.QueryString["Variable"]);
-        }
+		[Test]
+		public void CanSpecifyQueryString()
+		{
+			TestControllerBuilder handler = new TestControllerBuilder();
+			handler.QueryString["Variable"] = "Value";
+			TestHelperController testController = handler.CreateController<TestHelperController>();
+			Assert.AreEqual("Value", testController.Request.QueryString["Variable"]);
+		}
 
-        [Test]
-        public void RedirectRecordsData()
-        {
-            TestControllerBuilder builder = new TestControllerBuilder();
-            TestHelperController testController = builder.CreateController<TestHelperController>();
-            testController.RedirectWithAction();
-            Assert.AreEqual("ActionName1", builder.RedirectToActionData.ActionName);
-            testController.RedirectWithActionAndController();
-            Assert.AreEqual("ActionName2", builder.RedirectToActionData.ActionName);
-            Assert.AreEqual("ControllerName2", builder.RedirectToActionData.ControllerName);
-            testController.RedirectWithObject();
-            Assert.AreEqual("ActionName3", builder.RedirectToActionData.ActionName);
-            Assert.AreEqual("ControllerName3", builder.RedirectToActionData.ControllerName);
-        }
+		[Test]
+		public void RedirectRecordsData()
+		{
+			TestControllerBuilder builder = new TestControllerBuilder();
+			TestHelperController testController = builder.CreateController<TestHelperController>();
+			testController.RedirectWithAction();
+			Assert.AreEqual("ActionName1", builder.RedirectToActionData.ActionName);
+			testController.RedirectWithActionAndController();
+			Assert.AreEqual("ActionName2", builder.RedirectToActionData.ActionName);
+			Assert.AreEqual("ControllerName2", builder.RedirectToActionData.ControllerName);
+			testController.RedirectWithObject();
+			Assert.AreEqual("ActionName3", builder.RedirectToActionData.ActionName);
+			Assert.AreEqual("ControllerName3", builder.RedirectToActionData.ControllerName);
+		}
 
-        [Test]
-        public void RenderViewRecordsData()
-        {
-            TestControllerBuilder builder = new TestControllerBuilder();
-            TestHelperController testController = builder.CreateController<TestHelperController>();
-            testController.RenderViewWithViewName();
-            Assert.AreEqual("View1", builder.RenderViewData.ViewName);
-            testController.RenderViewWithViewNameAndData();
-            Assert.AreEqual("View2", builder.RenderViewData.ViewName);
-            Assert.AreEqual(new { Prop1 = 1, Prop2 = 2 }, builder.RenderViewData.ViewData);
-            testController.RenderViewWithViewNameAndMaster();
-            Assert.AreEqual("View3", builder.RenderViewData.ViewName);
-            Assert.AreEqual("Master3", builder.RenderViewData.MasterName);
-            testController.RenderViewWithViewNameAndMasterAndData();
-            Assert.AreEqual("View4", builder.RenderViewData.ViewName);
-            Assert.AreEqual("Master4", builder.RenderViewData.MasterName);
-            Assert.AreEqual(new { Prop1 = 3, Prop2 = 4 }, builder.RenderViewData.ViewData);
-        }
+		[Test]
+		public void RenderViewRecordsData()
+		{
+			TestControllerBuilder builder = new TestControllerBuilder();
+			TestHelperController testController = builder.CreateController<TestHelperController>();
+			testController.RenderViewWithViewName();
+			Assert.AreEqual("View1", builder.RenderViewData.ViewName);
+			testController.RenderViewWithViewNameAndData();
+			Assert.AreEqual("View2", builder.RenderViewData.ViewName);
+			Assert.AreEqual(new { Prop1 = 1, Prop2 = 2 }, builder.RenderViewData.ViewData);
+			testController.RenderViewWithViewNameAndMaster();
+			Assert.AreEqual("View3", builder.RenderViewData.ViewName);
+			Assert.AreEqual("Master3", builder.RenderViewData.MasterName);
+			testController.RenderViewWithViewNameAndMasterAndData();
+			Assert.AreEqual("View4", builder.RenderViewData.ViewName);
+			Assert.AreEqual("Master4", builder.RenderViewData.MasterName);
+			Assert.AreEqual(new { Prop1 = 3, Prop2 = 4 }, builder.RenderViewData.ViewData);
+		}
 
-        [Test]
-        public void CanCreateMultipleControllers()
-        {
-            TestControllerBuilder builder = new TestControllerBuilder();
-            TestHelperController testController = builder.CreateController<TestHelperController>();
-            TestHelperController testController2 = builder.CreateController<TestHelperController>();
-            Assert.AreNotEqual(testController, testController2);
-        }
+		[Test]
+		public void CanCreateMultipleControllers()
+		{
+			TestControllerBuilder builder = new TestControllerBuilder();
+			TestHelperController testController = builder.CreateController<TestHelperController>();
+			TestHelperController testController2 = builder.CreateController<TestHelperController>();
+			Assert.AreNotEqual(testController, testController2);
+		}
 
-        [Test]
-        public void InterceptorShouldIgnoreNormalMethods()
-        {
-            TestControllerBuilder builder = new TestControllerBuilder();
-            TestHelperController testController = builder.CreateController<TestHelperController>();
-            int x = testController.RandomOtherFunction();
-            Assert.AreEqual(12345, x);
-        }
-    }
+		[Test]
+		public void InterceptorShouldIgnoreNormalMethods()
+		{
+			TestControllerBuilder builder = new TestControllerBuilder();
+			TestHelperController testController = builder.CreateController<TestHelperController>();
+			int x = testController.RandomOtherFunction();
+			Assert.AreEqual(12345, x);
+		}
+
+		[Test]
+		public void CanCreateControllersWithConstructorArgs()
+		{
+			var builder = new TestControllerBuilder();
+			var mocks = new MockRepository();
+			var mockService = mocks.CreateMock<ITestService>();
+			string moo = "moo";
+			using (mocks.Record())
+			{
+				Expect.Call(mockService.ReturnMoo()).Return(moo);
+			}
+
+			using (mocks.Playback())
+			{
+				var testController =
+					builder.CreateController<TestHelperControllerWithArgs>(mockService);
+				Assert.That(testController.ReturnMooFromService(), Is.EqualTo(moo));
+			}
+		}
+
+	}
 }
--- trunk/src/MvcContrib.TestHelper/MvcContrib.TestHelper/TestControllerBuilder.cs
+++ trunk/src/MvcContrib.TestHelper/MvcContrib.TestHelper/TestControllerBuilder.cs
@@ -2,193 +2,195 @@
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
+using Castle.Core.Interceptor;
 using Castle.DynamicProxy;
 using Rhino.Mocks;
 
 namespace MvcContrib.TestHelper
 {
-    /// <summary>
-    /// This is primary class used to create controllers.
-    /// After initializing, call CreateController to create a controller with proper environment elements.
-    /// Exposed properties such as Form, QueryString, and HttpContext allow access to text environment.
-    /// RenderViewData and RedirectToActionData record those methods
-    /// </summary>
-    public partial class TestControllerBuilder
-    {
-        protected MockRepository _mocks;
-        protected TempDataDictionary _tempData;
+	/// <summary>
+	/// This is primary class used to create controllers.
+	/// After initializing, call CreateController to create a controller with proper environment elements.
+	/// Exposed properties such as Form, QueryString, and HttpContext allow access to text environment.
+	/// RenderViewData and RedirectToActionData record those methods
+	/// </summary>
+	public partial class TestControllerBuilder
+	{
+		protected MockRepository _mocks;
+		protected TempDataDictionary _tempData;
 
-        /// <summary>
-        /// Initializes a new instance of the <see cref="TestControllerBuilder"/> class.
-        /// </summary>
-        public TestControllerBuilder()
-        {
-            AppRelativeCurrentExecutionFilePath = "~/";
-            ApplicationPath = "/";
-            PathInfo = "";
-            RouteData = new RouteData();
-            _mocks = new MockRepository();
-            Session = new MockSession();
-            SetupHttpContext();
-        }
+		/// <summary>
+		/// Initializes a new instance of the <see cref="TestControllerBuilder"/> class.
+		/// </summary>
+		public TestControllerBuilder()
+		{
+			AppRelativeCurrentExecutionFilePath = "~/";
+			ApplicationPath = "/";
+			PathInfo = "";
+			RouteData = new RouteData();
+			_mocks = new MockRepository();
+			Session = new MockSession();
+			SetupHttpContext();
+		}
 
-        /// <summary>
-        /// Gets the data object created by a controller calling RedirectToAction
-        /// </summary>
-        /// <value>The RedirectToAction data.</value>
-        public RedirectToActionData RedirectToActionData
-        {
-            get;
-            protected set;
-        }
+		/// <summary>
+		/// Gets the data object created by a controller calling RedirectToAction
+		/// </summary>
+		/// <value>The RedirectToAction data.</value>
+		public RedirectToActionData RedirectToActionData
+		{
+			get;
+			protected set;
+		}
 
-        /// <summary>
-        /// Gets the data object created by a controller calling RenderView
-        /// </summary>
-        /// <value>The RenderViewData data.</value>
-        public RenderViewData RenderViewData
-        {
-            get;
-            protected set;
-        }
+		/// <summary>
+		/// Gets the data object created by a controller calling RenderView
+		/// </summary>
+		/// <value>The RenderViewData data.</value>
+		public RenderViewData RenderViewData
+		{
+			get;
+			protected set;
+		}
 
-        /// <summary>
-        /// Gets the HttpContext that built controllers will have set internally when created with CreateController
-        /// </summary>
-        /// <value>The HTTPContext</value>
-        public HttpContextBase HttpContext
-        {
-            get;
-            protected set;
-        }
+		/// <summary>
+		/// Gets the HttpContext that built controllers will have set internally when created with CreateController
+		/// </summary>
+		/// <value>The HTTPContext</value>
+		public HttpContextBase HttpContext
+		{
+			get;
+			protected set;
+		}
 
-        /// <summary>
-        /// Gets the Form data that built controllers will have set internally when created with CreateController
-        /// </summary>
-        /// <value>The NameValueCollection Form</value>
-        public NameValueCollection Form
-        {
-            get;
-            protected set;
-        }
+		/// <summary>
+		/// Gets the Form data that built controllers will have set internally when created with CreateController
+		/// </summary>
+		/// <value>The NameValueCollection Form</value>
+		public NameValueCollection Form
+		{
+			get;
+			protected set;
+		}
 
-        /// <summary>
-        /// Gets the QueryString that built controllers will have set internally when created with CreateController
-        /// </summary>
-        /// <value>The NameValueCollection QueryString</value>
-        public NameValueCollection QueryString
-        {
-            get;
-            protected set;
-        }
+		/// <summary>
+		/// Gets the QueryString that built controllers will have set internally when created with CreateController
+		/// </summary>
+		/// <value>The NameValueCollection QueryString</value>
+		public NameValueCollection QueryString
+		{
+			get;
+			protected set;
+		}
 
-        /// <summary>
-        /// Gets the Session that built controllers will have set internally when created with CreateController
-        /// </summary>
-        /// <value>The IHttpSessionState Session</value>
-        public HttpSessionStateBase Session
-        { 
-            get;
-            protected set;
-        }
+		/// <summary>
+		/// Gets the Session that built controllers will have set internally when created with CreateController
+		/// </summary>
+		/// <value>The IHttpSessionState Session</value>
+		public HttpSessionStateBase Session
+		{
+			get;
+			protected set;
+		}
 
-        /// <summary>
-        /// Gets the TempDataDictionary that built controllers will have set internally when created with CreateController
-        /// </summary>
-        /// <value>The TempDataDictionary</value>
-        public TempDataDictionary TempDataDictionary
-        {
-            get;
-            protected set;
-        }
+		/// <summary>
+		/// Gets the TempDataDictionary that built controllers will have set internally when created with CreateController
+		/// </summary>
+		/// <value>The TempDataDictionary</value>
+		public TempDataDictionary TempDataDictionary
+		{
+			get;
+			protected set;
+		}
 
-        /// <summary>
-        /// Gets or sets the RouteData that built controllers will have set internally when created with CreateController
-        /// </summary>
-        /// <value>The RouteData</value>
-        public RouteData RouteData
-        {
-            get;
-            set;
-        }
+		/// <summary>
+		/// Gets or sets the RouteData that built controllers will have set internally when created with CreateController
+		/// </summary>
+		/// <value>The RouteData</value>
+		public RouteData RouteData
+		{
+			get;
+			set;
+		}
 
-        /// <summary>
-        /// Gets or sets the AppRelativeCurrentExecutionFilePath that built controllers will have set internally when created with CreateController
-        /// </summary>
-        /// <value>The RouteData</value>
-        public string AppRelativeCurrentExecutionFilePath
-        {
-            get;
-            set;
-        }
+		/// <summary>
+		/// Gets or sets the AppRelativeCurrentExecutionFilePath that built controllers will have set internally when created with CreateController
+		/// </summary>
+		/// <value>The RouteData</value>
+		public string AppRelativeCurrentExecutionFilePath
+		{
+			get;
+			set;
+		}
 
-        /// <summary>
-        /// Gets or sets the AppRelativeCurrentExecutionFilePath string that built controllers will have set internally when created with CreateController
-        /// </summary>
-        /// <value>The ApplicationPath string</value>
-        public string ApplicationPath
-        {
-            get;
-            set;
-        }
+		/// <summary>
+		/// Gets or sets the AppRelativeCurrentExecutionFilePath string that built controllers will have set internally when created with CreateController
+		/// </summary>
+		/// <value>The ApplicationPath string</value>
+		public string ApplicationPath
+		{
+			get;
+			set;
+		}
 
-        /// <summary>
-        /// Gets or sets the PathInfo string that built controllers will have set internally when created with CreateController
-        /// </summary>
-        /// <value>The PathInfo string</value>
-        public string PathInfo
-        {
-            get;
-            set;
-        }
+		/// <summary>
+		/// Gets or sets the PathInfo string that built controllers will have set internally when created with CreateController
+		/// </summary>
+		/// <value>The PathInfo string</value>
+		public string PathInfo
+		{
+			get;
+			set;
+		}
 
-        protected void SetupHttpContext()
-        {
-            HttpContext = _mocks.DynamicMock<HttpContextBase>();
-            HttpRequestBase request = _mocks.DynamicMock<HttpRequestBase>();
-            HttpResponseBase response = _mocks.DynamicMock<HttpResponseBase>();
-            HttpServerUtilityBase server = _mocks.DynamicMock<HttpServerUtilityBase>();
+		protected void SetupHttpContext()
+		{
+			HttpContext = _mocks.DynamicMock<HttpContextBase>();
+			HttpRequestBase request = _mocks.DynamicMock<HttpRequestBase>();
+			HttpResponseBase response = _mocks.DynamicMock<HttpResponseBase>();
+			HttpServerUtilityBase server = _mocks.DynamicMock<HttpServerUtilityBase>();
 
-            SetupResult.For(HttpContext.Request).Return(request);
-            SetupResult.For(HttpContext.Response).Return(response);
-            SetupResult.For(HttpContext.Session).Return(Session);
-            SetupResult.For(HttpContext.Server).Return(server);
+			SetupResult.For(HttpContext.Request).Return(request);
+			SetupResult.For(HttpContext.Response).Return(response);
+			SetupResult.For(HttpContext.Session).Return(Session);
+			SetupResult.For(HttpContext.Server).Return(server);
 
-            QueryString = new NameValueCollection();
-            SetupResult.For(request.QueryString).Return(QueryString);
+			QueryString = new NameValueCollection();
+			SetupResult.For(request.QueryString).Return(QueryString);
 
-            Form = new NameValueCollection();
-            SetupResult.For(request.Form).Return(Form);
+			Form = new NameValueCollection();
+			SetupResult.For(request.Form).Return(Form);
 
-            SetupResult.For(request.AppRelativeCurrentExecutionFilePath).Return(AppRelativeCurrentExecutionFilePath);
-            SetupResult.For(request.ApplicationPath).Return(ApplicationPath);
-            SetupResult.For(request.PathInfo).Return(PathInfo);
+			SetupResult.For(request.AppRelativeCurrentExecutionFilePath).Return(AppRelativeCurrentExecutionFilePath);
+			SetupResult.For(request.ApplicationPath).Return(ApplicationPath);
+			SetupResult.For(request.PathInfo).Return(PathInfo);
 
-            _mocks.Replay(HttpContext);
-            _mocks.Replay(request);
-            _mocks.Replay(response);
+			_mocks.Replay(HttpContext);
+			_mocks.Replay(request);
+			_mocks.Replay(response);
 
-            TempDataDictionary = new TempDataDictionary(HttpContext);
-        }
+			TempDataDictionary = new TempDataDictionary(HttpContext);
+		}
 
-        /// <summary>
-        /// Creates the controller with proper environment variables setup. 
-        /// </summary>
-        /// <typeparam name="T">The type of controller to create</typeparam>
-        /// <returns>A new controller of the specified type</returns>
-        public T CreateController<T>() where T : Controller
-        {
-            ProxyGenerator generator = new ProxyGenerator(); //Castle DynamicProxy
-            Controller controller = (Controller)generator.CreateClassProxy(typeof(T), new ControllerInterceptor(this));
+		/// <summary>
+		/// Creates the controller with proper environment variables setup. 
+		/// </summary>
+		/// <typeparam name="T">The type of controller to create</typeparam>
+		/// <param name="constructorArgs">Arguments to pass to the constructor for the controller</param>
+		/// <returns>A new controller of the specified type</returns>
+		public T CreateController<T>(params object[] constructorArgs) where T : Controller
+		{
+			ProxyGenerator generator = new ProxyGenerator(); //Castle DynamicProxy
+			Controller controller = (Controller)generator.CreateClassProxy(typeof(T), new IInterceptor[] { new ControllerInterceptor(this) }, constructorArgs);
 
-            ControllerContext controllerContext = new ControllerContext(HttpContext, RouteData, controller);
-            controller.ControllerContext = controllerContext;
-                //normally set in the internal function controller.Execute();
+			ControllerContext controllerContext = new ControllerContext(HttpContext, RouteData, controller);
+			controller.ControllerContext = controllerContext;
+			//normally set in the internal function controller.Execute();
 
-            typeof(T).GetProperty("TempData").SetValue(controller, TempDataDictionary, null);
-                //also set in controller.Execute();
+			typeof(T).GetProperty("TempData").SetValue(controller, TempDataDictionary, null);
+			//also set in controller.Execute();
 
-            return controller as T;
-        }
-    }
+			return controller as T;
+		}
+	}
 }
