d484552ca937671b8f8b3c34f1560a61498a672c
mvccontrib <mvccontrib@eb32e4ba-b140-0410-9c0e-b965c4cb1785>
2008-03-26 20:32:02 +0000

    CCNET build 91

---
+++ tags/91/src/MVCContrib.UnitTests/BrailViewEngine/TestHttpContext.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.IO;
+using System.Security.Principal;
+using System.Text;
+using System.Web;
+using System.Web.Caching;
+using System.Web.Profile;
+using System.Web.SessionState;
+using HttpSessionStateBase = System.Web.HttpSessionStateBase;
+
+namespace MvcContrib.UnitTests.BrailViewEngine
+{
+	public class TestHttpContext : HttpContextBase
+	{
+	}
+
+	public class TestHttpHandler : IHttpHandler
+	{
+		private bool _IsReusable;
+
+		public void ProcessRequest(HttpContext context)
+		{
+			throw new NotImplementedException();
+		}
+
+		public bool IsReusable
+		{
+			get { return _IsReusable; }
+			set { _IsReusable = value; }
+		}
+	}
+
+	public class TestHttpRequest : HttpRequestBase
+	{
+	}
+
+	public class TestHttpResponse : HttpResponseBase{
+
+	}
+
+	public class TestHttpServerUtility : HttpServerUtilityBase
+	{
+	}
+
+	public class TestHttpSessionState :  HttpSessionStateBase
+	{
+	}
+}
---
+++ tags/91/src/Samples/MvcContrib.Samples.BrailViewEngine/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Web.Mvc;
+
+namespace MvcContrib.Samples
+{
+	public class HomeController : Controller
+	{
+		
+		public void Index()
+		{
+			RenderView("Index", "Site");
+		}
+
+		
+		public void Contact()
+		{
+			CompanyInfo companyInfo = new CompanyInfo();
+			companyInfo.CompanyName = "Your company name here";
+			companyInfo.AddressLine1 = "Company address Line 1";
+			companyInfo.AddressLine2 = "Company address Line 2";
+			companyInfo.City = "City";
+			companyInfo.State = "State";
+			companyInfo.Zip = "00000";
+			companyInfo.Email = "email@yourcompany.com";
+			
+			RenderView("Contact", "Site", companyInfo);
+		}
+
+		
+		public void About()
+		{
+			ViewData["now"] = DateTime.Now;
+
+			RenderView("About", "Site");
+		}
+	}
+}
