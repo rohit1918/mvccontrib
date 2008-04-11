2566cb443256726868abab2b1fd5d4c426bba999
eric.hexter <eric.hexter@eb32e4ba-b140-0410-9c0e-b965c4cb1785>
2008-04-11 03:52:10 +0000

    Patch 1084 Added support for the ninject IoC container. Added support for the IoCControllerFactory & the NinjectControllerFactory.  Sample is included for this container.

---
+++ src/MvcContrib.Ninject/NinjectDependencyResolver.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using MvcContrib.Interfaces;
+using Ninject.Core;
+
+namespace MvcContrib.Ninject
+{
+    public class NinjectDependencyResolver : IDependencyResolver
+    {
+        private IKernel _kernel;
+
+        public NinjectDependencyResolver(IKernel kernel)
+        {
+            _kernel = kernel;
+        }
+
+        public Interface GetImplementationOf<Interface>()
+        {
+            return GetImplementationOf<Interface>(typeof(Interface));
+        }
+
+        public Interface GetImplementationOf<Interface>(Type type)
+        {
+            return (Interface)GetImplementationOf(type);
+        }
+
+        public object GetImplementationOf(Type type)
+        {
+            try
+            {
+                return _kernel.Get(type);
+            }
+            catch(ActivationException)
+            {
+                return null;
+            }
+        }
+    }
+}
