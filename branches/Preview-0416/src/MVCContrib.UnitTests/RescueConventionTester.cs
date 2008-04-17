2ee58ecdfe2a79608ef4efbf41a3cd186f5dc503
Jeremy.Skinner <Jeremy.Skinner@eb32e4ba-b140-0410-9c0e-b965c4cb1785>
2008-04-17 09:38:00 +0000

    Branching for the ASP.NET 0416 MVC Interim Source Code Release.

---
+++ branches/Preview-0416/src/MVCContrib.UnitTests/RescueConventionTester.cs
@@ -0,0 +1,185 @@
+using System;
+using System.Reflection;
+using System.Web.Mvc;
+using System.Web.Routing;
+using MvcContrib;
+using MvcContrib.Attributes;
+using NUnit.Framework;
+using NUnit.Framework.SyntaxHelpers;
+using Rhino.Mocks;
+using System.Web;
+
+namespace MvcContrib.UnitTests
+{
+	[TestFixture]
+	[Category("RescueConventionTester")]
+	public class When_ConventionController_is_decorated_with_Rescue_attribute_and_exception_occurs
+	{
+		private RescueTestController _rescueTestControllerDecorated;
+
+		[Test]
+		public void Then_controller_should_render_the_RescueAttribute_view()
+		{
+			string expectedRescueView = "Rescues/TestRescue";
+
+			Assert.That(_rescueTestControllerDecorated.ViewRendered, Is.EqualTo(expectedRescueView));
+		}
+
+		[Test]
+		public void And_the_onPreRescue_method_should_be_invoked()
+		{
+			Assert.That(_rescueTestControllerDecorated.onPreRescueFired, Is.True);
+		}
+
+		[SetUp]
+		public void SetUpContext()
+		{
+			MockRepository mocks = new MockRepository();
+			HttpContextBase context = mocks.DynamicMock<HttpContextBase>();
+			mocks.Replay(context);
+			_rescueTestControllerDecorated = new RescueTestController();
+			_rescueTestControllerDecorated.ControllerContext = new ControllerContext(context, new RouteData(), _rescueTestControllerDecorated);
+			string publicActionName = "ThrowError";
+			_rescueTestControllerDecorated.InvokeActionPublic(publicActionName);
+		}
+	}
+
+	[TestFixture]
+	[Category("RescueConventionTester")]
+	public class When_ConventionController_method_is_decorated_with_Rescue_attribute_and_exception_occurs
+	{
+		[Test]
+		public void Then_controller_should_render_method_RescueAttribute_view()
+		{
+			RescueTestController rescueTestControllerDecorated = new RescueTestController();
+			SetControllerContext(rescueTestControllerDecorated);
+
+			string publicActionName = "ThrowMethodError";
+			rescueTestControllerDecorated.InvokeActionPublic(publicActionName);
+
+			string expectedRescueView = "Rescues/TestMethodRescue";
+
+			Assert.That(rescueTestControllerDecorated.ViewRendered, Is.EqualTo(expectedRescueView));
+		}
+
+		public void SetControllerContext(Controller controller)
+		{
+			MockRepository mocks = new MockRepository();
+			HttpContextBase context = mocks.DynamicMock<HttpContextBase>();
+			mocks.Replay(context);
+			controller.ControllerContext = new ControllerContext(context, new RouteData(), controller);
+		}
+	}
+
+	[TestFixture]
+	[Category("RescueConventionTester")]
+	public class When_ConventionController_method_is_decorated_with_Rescue_attribute_and_an_ExceptionType_is_specified
+	{
+		[Test]
+		public void Then_controller_should_render_method_RescueAttribute_view_for_only_the_ExceptionType_specified()
+		{
+			RescueTestControllerWithExceptionTypes controller = new RescueTestControllerWithExceptionTypes();
+			SetControllerContext(controller);
+
+			string publicActionName = "ThrowError";
+			controller.InvokeActionPublic(publicActionName);
+
+			string expectedRescueView = "Rescues/Test";
+
+			Assert.That(controller.ViewRendered, Is.EqualTo(expectedRescueView));
+		}
+
+		[Test]
+		public void Then_controller_Should_not_rescue_the_action_if_the_incorrect_ExceptionType_is_thrown()
+		{
+			RescueTestControllerWithExceptionTypes controller = new RescueTestControllerWithExceptionTypes();
+			SetControllerContext(controller);
+
+			string publicActionName = "DontDoThis";
+
+			try
+			{
+				controller.InvokeActionPublic(publicActionName);
+			}
+			catch(TargetInvocationException exc)
+			{
+				Assert.IsAssignableFrom(typeof(NotImplementedException), exc.InnerException);
+			}
+		}
+
+		public void SetControllerContext(Controller controller)
+		{
+			MockRepository mocks = new MockRepository();
+			HttpContextBase context = mocks.DynamicMock<HttpContextBase>();
+			mocks.Replay(context);
+			controller.ControllerContext = new ControllerContext(context, new RouteData(), controller);
+		}
+	}
+
+	[Rescue("TestRescue")]
+	internal class RescueTestController : BaseRescueTestController
+	{
+		public bool onPreRescueFired;
+
+		public void ThrowError()
+		{
+			throw new NotImplementedException();
+		}
+
+		[Rescue("TestMethodRescue")]
+		public void ThrowMethodError()
+		{
+			throw new NotImplementedException();
+		}
+
+		protected override void OnPreRescue(Exception exception)
+		{
+			onPreRescueFired = true;
+			base.OnPreRescue(exception);
+		}
+	}
+
+	[Rescue("Test", typeof(RescueTestException))]
+	internal class RescueTestControllerWithExceptionTypes : BaseRescueTestController
+	{
+		public void ThrowError()
+		{
+			throw new RescueTestException();
+		}
+
+		[Rescue("Test", typeof(RescueTestException))]
+		public void ThrowMethodError()
+		{
+			throw new RescueTestException();
+		}
+
+		[Rescue("Test", typeof(RescueTestException))]
+		public void DontDoThis()
+		{
+			throw new NotImplementedException();
+		}
+	}
+
+	internal abstract class BaseRescueTestController : ConventionController
+	{
+		protected override void RenderView(string viewName, string masterName, object viewData)
+		{
+			this.ViewRendered = viewName;
+		}
+
+		public string ViewRendered
+		{
+			get;
+			private set;
+		}
+
+		public bool InvokeActionPublic(string actionName)
+		{
+			return base.InvokeAction(actionName);
+		}
+	}
+
+	internal class RescueTestException : Exception
+	{
+	}
+}
---
+++ branches/Preview-0416/src/MvcContrib.NHamlViewEngine/Rules/TagMarkupRule.cs
@@ -0,0 +1,198 @@
+using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using MvcContrib.NHamlViewEngine.Exceptions;
+using MvcContrib.NHamlViewEngine.Properties;
+using System.Text;
+
+namespace MvcContrib.NHamlViewEngine.Rules
+{
+	public class TagMarkupRule : MarkupRule
+	{
+		private const string Id = "id";
+		private const string Class = "class";
+
+		private static readonly Regex _tagRegex = new Regex(
+			@"^([-:\w]+)([-\w\.\#]*)\s*(\{(.*)\})?([\/=]?)(.*)$",
+			RegexOptions.Compiled | RegexOptions.Singleline);
+
+		private static readonly Regex _idClassesRegex = new Regex(
+			@"(?:(?:\#([-\w]+))|(?:\.([-\w]+)))+",
+			RegexOptions.Compiled | RegexOptions.Singleline);
+
+        private static readonly Regex _attributeNameReplacer = new Regex(
+            @"(?<Name>\@?\w+)\s?\=",
+            RegexOptions.Compiled | RegexOptions.Singleline);
+
+		private static List<string> WhitespaceSensitiveTags = new List<string>{ "textarea", "pre" };
+
+		protected virtual string PreprocessLine(InputLine inputLine)
+		{
+			return inputLine.NormalizedText;
+		}
+
+		public override char Signifier
+		{
+			get { return '%'; }
+		}
+
+		public override BlockClosingAction Render(CompilationContext compilationContext)
+		{
+			Match match = _tagRegex.Match(PreprocessLine(compilationContext.CurrentInputLine));
+
+			if(!match.Success)
+			{
+				SyntaxException.Throw(compilationContext.CurrentInputLine, Resources.ErrorParsingTag,
+				                      compilationContext.CurrentInputLine);
+			}
+
+			bool isWhitespaceSensitive = WhitespaceSensitiveTags.Contains(match.Groups[1].Value);
+
+			string openingTag = compilationContext.CurrentInputLine.Indent + '<' + match.Groups[1].Value;
+			string closingTag = "</" + match.Groups[1].Value + '>';
+
+			compilationContext.ViewBuilder.AppendOutput(openingTag);
+
+			ParseAndRenderAttributes(compilationContext, match);
+
+			string action = match.Groups[5].Value;
+
+			if(string.Equals("/", action)
+			   || compilationContext.TemplateCompiler.IsAutoClosing(match.Groups[1].Value))
+			{
+				compilationContext.ViewBuilder.AppendOutputLine(" />");
+
+				return null;
+			}
+			else
+			{
+				string content = match.Groups[6].Value.Trim();
+
+				if(string.IsNullOrEmpty(content))
+				{
+					compilationContext.ViewBuilder.AppendOutputLine(">");
+					closingTag = compilationContext.CurrentInputLine.Indent + closingTag;
+				}
+				else
+				{
+					if((content.Length > 50) || string.Equals("=", action))
+					{
+						compilationContext.ViewBuilder.AppendOutput(">", !isWhitespaceSensitive);
+                        if (!isWhitespaceSensitive)
+                        {
+                            compilationContext.ViewBuilder.AppendOutput(compilationContext.CurrentInputLine.Indent + "  ");
+                        }
+
+						if(string.Equals("=", action))
+						{
+							compilationContext.ViewBuilder.AppendCode(content, !isWhitespaceSensitive);
+						}
+						else
+						{
+							compilationContext.ViewBuilder.AppendOutput(content, !isWhitespaceSensitive);
+						}
+
+                        if (!isWhitespaceSensitive)
+                        {
+                            closingTag = compilationContext.CurrentInputLine.Indent + closingTag;
+                        }
+					}
+					else
+					{
+						compilationContext.ViewBuilder.AppendOutput(">" + content);
+					}
+				}
+
+				return delegate { compilationContext.ViewBuilder.AppendOutputLine(closingTag); };
+			}
+		}
+
+		private static void ParseAndRenderAttributes(CompilationContext compilationContext, Match tagMatch)
+		{
+			string idAndClasses = tagMatch.Groups[2].Value;
+			string attributesHash = tagMatch.Groups[4].Value.Trim();
+
+			Match match = _idClassesRegex.Match(idAndClasses);
+
+			List<string> classes = new List<string>();
+
+			foreach(Capture capture in match.Groups[2].Captures)
+			{
+				classes.Add(capture.Value);
+			}
+
+			if(classes.Count > 0)
+			{
+				attributesHash = PrependAttribute(attributesHash,
+				                                  '@' + Class, string.Join(" ", classes.ToArray()));
+			}
+
+			string id = null;
+
+			foreach(Capture capture in match.Groups[1].Captures)
+			{
+				id = capture.Value;
+			}
+
+			if(!string.IsNullOrEmpty(id))
+			{
+				attributesHash = PrependAttribute(attributesHash, Id, id);
+			}
+
+			if(!string.IsNullOrEmpty(attributesHash))
+			{
+				compilationContext.ViewBuilder.AppendOutput(" ");
+
+                attributesHash =_attributeNameReplacer.Replace(
+                                    attributesHash,
+                                    m => CheckAttributeName(m.Groups["Name"].Value) + " = "
+                                    );
+
+				string attributes = TryPrecompileAttributes(compilationContext.TemplateCompiler, attributesHash);
+
+				if(!string.IsNullOrEmpty(attributes))
+				{
+					compilationContext.ViewBuilder.AppendOutput(attributes);
+				}
+				else
+				{
+					compilationContext.ViewBuilder.AppendCode("new {" + attributesHash + "}.RenderAttributes()");
+				}
+			}
+		}
+
+		private static string TryPrecompileAttributes(TemplateCompiler templateCompiler, string attributesHash)
+		{
+			string source = "public class AttributeEvaluator:IAttributeEvaluator{ public string Eval(){return new {"
+			                + attributesHash + "}.RenderAttributes();}}";
+
+			Type type = new TypeBuilder(templateCompiler).Build(source, "AttributeEvaluator");
+
+			string attributes = null;
+
+			if(type != null)
+			{
+				attributes = ((IAttributeEvaluator)Activator.CreateInstance(type)).Eval();
+			}
+
+			return attributes;
+		}
+
+        private static readonly List<string> keywords = new List<string>{ "class", "new", "void", "public", "for" }; //.. etc
+        private static string CheckAttributeName(string name)
+        {
+            if (keywords.Contains(name))
+            {
+                return "@" + name;
+            }
+            return name;
+        }
+
+		private static string PrependAttribute(string attributesHash, string name, string value)
+		{
+			string attribute = name + "=\"" + value + "\"";
+
+			return string.IsNullOrEmpty(attributesHash) ? attribute : attribute + "," + attributesHash;
+		}
+	}
+}
