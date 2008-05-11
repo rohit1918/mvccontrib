c947718f852876e74363dca3a3b3284688a0dbfc
mvccontrib <mvccontrib@eb32e4ba-b140-0410-9c0e-b965c4cb1785>
2008-05-11 20:19:42 +0000

    CCNET build 112

---
+++ tags/112/src/MVCContrib.UnitTests/NHamlViewEngine/NHamlConfigurationTester.cs
@@ -0,0 +1,60 @@
+﻿using MvcContrib.NHamlViewEngine.Configuration;
+using NUnit.Framework;
+
+namespace MvcContrib.UnitTests.NHamlViewEngine
+{
+	[TestFixture, Category("NHamlViewEngine")]
+	public class ConfigurationTester
+	{
+		[Test]
+		public void Can_Read_Production_Setting_From_AppSettings()
+		{
+			NHamlViewEngineSection section = NHamlViewEngineSection.Read();
+
+			Assert.IsNotNull(section);
+			Assert.IsFalse(section.Production);
+		}
+
+		[Test]
+		public void Can_Read_Views_Assemblies_Section_From_AppSettings()
+		{
+			NHamlViewEngineSection section = NHamlViewEngineSection.Read();
+			Assert.IsNotNull(section);
+
+			AssemblyConfigurationElement assembly = section.Views.Assemblies[0];
+			Assert.AreEqual(1, section.Views.Assemblies.Count);
+			Assert.AreEqual("System, Version=2.0.0.0, Culture=neutral, PublicKeyToken=B77A5C561934E089", assembly.Name);
+		}
+
+		[Test]
+		public void Can_Read_Views_Namespaces_Section_From_AppSettings()
+		{
+			NHamlViewEngineSection section = NHamlViewEngineSection.Read();
+			Assert.IsNotNull(section);
+
+			NamespaceConfigurationElement _namespace = section.Views.Namespaces[0];
+			Assert.AreEqual(1, section.Views.Namespaces.Count);
+			Assert.AreEqual("System.Collections", _namespace.Name);
+		}
+
+		[Test]
+		public void ForCoverage()
+		{
+			new AssemblyConfigurationElement("Name").Name = "Name";
+			new NamespaceConfigurationElement("Name").Name = "Name";
+
+			NamespacesConfigurationCollection ncc = new NamespacesConfigurationCollection();
+			ncc.Add(new NamespaceConfigurationElement("Name"));
+			NamespaceConfigurationElement nce = ncc["Name"];
+			ncc.IndexOf(nce);
+			ncc.Remove(nce);
+			ncc.Add(nce);
+			ncc.RemoveAt(0);
+			ncc.Add(nce);
+			ncc.Remove("Name");
+			ncc.Clear();
+			ncc.Add(nce);
+			ncc[0] = nce;
+		}
+	}
+}
