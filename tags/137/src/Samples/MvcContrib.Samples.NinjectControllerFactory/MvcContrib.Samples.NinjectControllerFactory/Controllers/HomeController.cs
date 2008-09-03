eb661d4dc334c133cb2d19f115333d2a3982fd63
mvccontrib <mvccontrib@eb32e4ba-b140-0410-9c0e-b965c4cb1785>
2008-09-03 01:26:16 +0000

    CCNET build 137

---
+++ tags/137/src/Samples/MvcContrib.Samples.NinjectControllerFactory/MvcContrib.Samples.NinjectControllerFactory/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System.Web.Mvc;
+using NinjectControllerFactory.Domain;
+
+namespace MvcContrib.Samples.NinjectControllerFactory.Controllers
+{
+    public class HomeController : Controller
+    {
+        private Samurai _samurai;
+
+        public HomeController(Samurai samurai)
+        {
+            _samurai = samurai;
+        }
+
+        public ActionResult Index()
+        {
+            ViewData["attackresult"] = _samurai.Attack("the evildoers");
+            return View("index");
+        }
+    }
+}
