f786bdc11216335d9401986805b1d18bfe35f46e
mvccontrib <mvccontrib@eb32e4ba-b140-0410-9c0e-b965c4cb1785>
2008-09-05 12:16:34 +0000

    CCNET build 139

---
+++ tags/139/src/MvcContrib.ObjectBuilder/ObjectBuilderControllerFactory.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Web.Mvc;
+using System.Web.Routing;
+using DefaultControllerFactory=MvcContrib.ControllerFactories.DefaultControllerFactory;
+
+namespace MvcContrib.ObjectBuilder
+{
+    public class ObjectBuilderControllerFactory : DefaultControllerFactory
+    {
+        public override IController CreateController(RequestContext context, string controllerName)
+        {
+            Type type = base.GetControllerType(controllerName);
+
+            if(type == null)
+            {
+                throw new InvalidOperationException(string.Format("Could not find a controller with the name {0}", controllerName));
+            }
+
+            IDependencyContainer container = GetContainer(context);
+            return (IController)container.Get(type);
+        }
+
+        protected virtual IDependencyContainer GetContainer(RequestContext context)
+        {
+            if (context == null)
+            {
+                throw new ArgumentNullException("context");
+            }
+
+            var dependencyContainerAccessor = context.HttpContext.ApplicationInstance as IDependencyContainerAccessor;
+            if (dependencyContainerAccessor == null)
+            {
+                throw new InvalidOperationException(
+                    "You must extend the HttpApplication in your web project and implement the IContainerAccessor to properly expose your container instance");
+            }
+
+            IDependencyContainer container = dependencyContainerAccessor.Container;
+            if (container == null)
+            {
+                throw new InvalidOperationException("The container seems to be unavailable in your HttpApplication subclass");
+            }
+
+            return container;
+        }
+    }
+}
