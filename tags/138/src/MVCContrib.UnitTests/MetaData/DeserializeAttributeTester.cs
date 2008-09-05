3a952a172123b748e712213552c146068fd84efc
mvccontrib <mvccontrib@eb32e4ba-b140-0410-9c0e-b965c4cb1785>
2008-09-05 02:17:28 +0000

    CCNET build 138

---
+++ tags/138/src/MVCContrib.UnitTests/MetaData/DeserializeAttributeTester.cs
@@ -0,0 +1,67 @@
+using System.Web.Mvc;
+using System.Web.Routing;
+using MvcContrib.Attributes;
+using NUnit.Framework;
+using Rhino.Mocks;
+
+namespace MvcContrib.UnitTests.MetaData
+{
+	[TestFixture]
+	public class DeserializeAttributeTester
+	{
+		private MockRepository _mocks;
+		private ControllerContext _controllerContext;
+
+		[SetUp]
+		public void SetUp()
+		{
+			_mocks = new MockRepository();
+			var context = _mocks.DynamicHttpContextBase();
+			_mocks.Replay(context.Request);
+
+			context.Request.QueryString["ids[0]"] = "1";
+			context.Request.Form["ids[1]"] = "2";
+
+			var requestContext = new RequestContext(context, new RouteData());
+			_controllerContext = new ControllerContext(requestContext, _mocks.DynamicMock<ControllerBase>());
+		}
+
+		[Test]
+		public void CanCreateAttribute()
+		{
+			var attr = new DeserializeAttribute("ids", RequestStore.Params);
+		}
+
+		[Test]
+		public void CanDeserializeFromQueryString()
+		{
+			var attr = new DeserializeAttribute("ids", RequestStore.QueryString);
+
+			var ids = (int[])attr.GetValue(_controllerContext, null, typeof(int[]), null);
+			Assert.IsNotNull(ids);
+			Assert.AreEqual(1, ids.Length);
+			Assert.AreEqual(1, ids[0]);
+		}
+
+		[Test]
+		public void CanDeserializeFromForm()
+		{
+			var attr = new DeserializeAttribute("ids", RequestStore.Form);
+
+			var ids = (int[])attr.GetValue(_controllerContext, null, typeof(int[]), null);
+			Assert.IsNotNull(ids);
+			Assert.AreEqual(1, ids.Length);
+			Assert.AreEqual(2, ids[0]);
+		}
+
+		[Test]
+		public void CanDeserializeFromParams()
+		{
+			var attr = new DeserializeAttribute("ids", RequestStore.Params);
+
+			var ids = (int[])attr.GetValue(_controllerContext, null, typeof(int[]), null);
+			Assert.IsNotNull(ids);
+			Assert.AreEqual(2, ids.Length);
+		}
+	}
+}
