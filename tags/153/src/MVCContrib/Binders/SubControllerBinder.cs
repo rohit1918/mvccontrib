44f77b8b86087fb093085e0cbc16382868c5e50b
mvccontrib <mvccontrib@eb32e4ba-b140-0410-9c0e-b965c4cb1785>
2008-09-09 18:01:21 +0000

    CCNET build 153

---
+++ tags/153/src/MVCContrib/Binders/SubControllerBinder.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Globalization;
+using System.Web.Mvc;
+
+namespace MvcContrib.Binders
+{
+	///<summary>
+	/// Binder that creates SubControllers that are needed for an action method
+	///</summary>
+	public class SubControllerBinder : DefaultModelBinder
+	{
+		protected override object ConvertType(CultureInfo culture, object value, Type destinationType)
+		{
+			if (typeof (ISubController).IsAssignableFrom(destinationType))
+			{
+				object instance = CreateSubController(destinationType);
+				if (instance == null)
+				{
+					throw new InvalidOperationException(destinationType + " not created properly.");
+				}
+
+				return instance;
+			}
+
+			return base.ConvertType(culture, value, destinationType);
+		}
+
+		///<summary>
+		/// Creates the subcontroller given its type.  Override this method to wire into an IoC container
+		///</summary>
+		///<param name="destinationType">The type of subcontroller</param>
+		///<returns>an object instance</returns>
+		public virtual object CreateSubController(Type destinationType)
+		{
+			return Activator.CreateInstance(destinationType, true);
+		}
+	}
+}
---
+++ tags/153/src/MvcContrib.Castle/CastleBinder.cs
@@ -0,0 +1,85 @@
+using System;
+using System.Web.Mvc;
+using Castle.Components.Binder;
+using MvcContrib.Attributes;
+using MvcContrib.MetaData;
+
+namespace MvcContrib.Castle
+{
+	/// <summary>
+	/// Parameter binder that uses the Castle DataBinder to bind action parameters.
+	/// Example:
+	/// <![CDATA[
+	/// public ActionResult Save([CastleBind] Customer customer) {
+	///  //...
+	/// }
+	/// ]]>
+	/// </summary>
+	[AttributeUsage(AttributeTargets.Parameter, AllowMultiple = false, Inherited = false), Serializable]
+	public class CastleBindAttribute : AbstractParameterBinderAttribute
+	{
+		/// <summary>
+		/// Properties to exclude from binding
+		/// </summary>
+		public string Exclude { get; set; }
+
+		/// <summary>
+        /// Creates a new CastleBind attribute with the specified parameter prefix. 
+        /// </summary>
+        /// <param name="prefix">Prefix to use when extracting from the Request.Form.</param>
+        public CastleBindAttribute(string prefix) : base(prefix)
+        {
+        }
+
+        /// <summary>
+        /// Creates a new CastleBind attribute. The name of the parameter will be used as the request prefix.
+        /// </summary>
+		public CastleBindAttribute() : base(null)
+        {
+        }
+
+		/// <summary>
+		/// Performs the binding.
+		/// </summary>
+		/// <param name="controllerContext"></param>
+		/// <param name="modelName"></param>
+		/// <param name="modelType"></param>
+		/// <param name="modelState"></param>
+		/// <returns></returns>
+		public override object GetValue(ControllerContext controllerContext, string modelName, Type modelType, ModelStateDictionary modelState)
+		{
+			IDataBinder binder = LocateBinder(controllerContext);
+			object instance = binder.BindObject(modelType, Prefix ?? modelName, Exclude, null, new TreeBuilder().BuildSourceNode(controllerContext.HttpContext.Request.Form));
+			return instance;
+		}
+
+		/// <summary>
+		/// Finds the binder to use. If the controller implements ICastleBindingContainer then its binder is used. Otherwise, a new DataBinder is created.
+		/// </summary>
+		/// <returns></returns>
+		protected virtual IDataBinder LocateBinder(ControllerContext context)
+		{
+			var bindingContainer = context.Controller as ICastleBindingContainer;
+
+			if(bindingContainer != null)
+			{
+				if(bindingContainer.Binder == null)
+				{
+					bindingContainer.Binder = CreateBinder();
+				}
+				return bindingContainer.Binder;
+			}
+
+			return CreateBinder();
+		}
+
+        /// <summary>
+        /// Creates the binder to use.
+        /// </summary>
+        /// <returns>IDataBinder</returns>
+        protected virtual IDataBinder CreateBinder()
+        {
+            return new DataBinder();
+        }
+	}
+}
