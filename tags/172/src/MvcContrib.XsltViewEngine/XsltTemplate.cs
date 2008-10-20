07370ac9c84fbbfb313245ebd9cbfdba3daadd7d
mvccontrib <mvccontrib@eb32e4ba-b140-0410-9c0e-b965c4cb1785>
2008-10-20 11:34:36 +0000

    CCNET build 172

---
+++ tags/172/src/MvcContrib.XsltViewEngine/XsltTemplate.cs
@@ -0,0 +1,47 @@
+using System;
+using System.IO;
+using System.Xml;
+using MvcContrib.ViewFactories;
+using Mvp.Xml.Common.Xsl;
+
+namespace MvcContrib.XsltViewEngine
+{
+	public class XsltTemplate
+	{
+		private readonly MvpXslTransform _transform;
+        public string ViewUrl { get; private set; }
+		public XsltTemplate(IViewSourceLoader viewSourceLoader,string viewPath)
+		{
+
+			if( viewSourceLoader == null )
+			{
+				throw new ArgumentNullException("viewSourceLoader");
+			}
+
+			if( !viewSourceLoader.HasView(viewPath) )
+			{
+				throw new InvalidOperationException(string.Format("Couldn't find the template with name {0}.", viewPath));
+			}
+		    ViewUrl = viewPath;
+
+			IViewSource viewSource = viewSourceLoader.GetViewSource(viewPath);
+
+			_transform = new MvpXslTransform();
+
+			var settings = new XmlReaderSettings {ProhibitDtd = false};
+
+			using(var viewSourceStream = viewSource.OpenViewStream())
+			{
+				using (var xmlReader = XmlReader.Create(viewSourceStream, settings))
+				{
+					_transform.Load(xmlReader);
+				}
+			}
+		}
+
+		public MvpXslTransform XslTransformer
+		{
+			get { return _transform; }
+		}
+	}
+}
