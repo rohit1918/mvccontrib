719c9de0997140acfd86c6d873f43ffb1152a3ba
mvccontrib <mvccontrib@eb32e4ba-b140-0410-9c0e-b965c4cb1785>
2008-11-23 14:01:24 +0000

    CCNET build 183

---
+++ tags/183/src/MVCContrib.UnitTests/Binders/WindsorModelBinderTester.cs
@@ -0,0 +1,73 @@
+using System;
+using System.Web.Mvc;
+using System.Web.Routing;
+using Castle.Windsor;
+using MvcContrib.Castle;
+using NUnit.Framework;
+using NUnit.Framework.SyntaxHelpers;
+using Rhino.Mocks;
+
+namespace MvcContrib.UnitTests.Binders
+{
+	[TestFixture]
+	public class WindsorModelBinderTester
+	{
+		private ModelBindingContext _context;
+
+		[SetUp]
+		public void Setup()
+		{
+			var httpContext = MvcMockHelpers.DynamicHttpContextBase();
+			var controllerContext = new ControllerContext(httpContext, new RouteData(), MockRepository.GenerateStub<ControllerBase>());
+
+			_context = new ModelBindingContext(controllerContext, MockRepository.GenerateStub<IValueProvider>(), typeof(object), "Test", null, new ModelStateDictionary(), null);
+		}
+
+		[Test]
+		public void ShouldResolveTheCorrectBinder_WhenBinderExists()
+		{
+			IWindsorContainer container = new WindsorContainer();
+
+			container.AddComponent<IModelBinder, TestModelBinder>("testmodelbinder");
+
+			var binder = new WindsorModelBinder(container);
+
+			var value = binder.BindModel(_context);
+
+			Assert.That(value.Value, Is.EqualTo("TestResult"));
+		}
+
+		[Test]
+		public void ShouldFallbackToDefaultBinder_WhenBinderDoesNotExist()
+		{
+			var container = new WindsorContainer();
+			var fallbackBinder = MockRepository.GenerateMock<IModelBinder>();
+			fallbackBinder.Expect(b => b.BindModel(_context))
+				.Return(new ModelBinderResult("MockedResult"));
+
+			var binder = new WindsorModelBinder(container, fallbackBinder);
+
+			var value = binder.BindModel(_context);
+
+			Assert.That(value.Value, Is.EqualTo("MockedResult"));
+		}
+
+		[Test, ExpectedException(typeof(InvalidOperationException))]
+		public void ShouldThrow_WhenComponentIsNotIModelBinder()
+		{
+			var container = new WindsorContainer();
+			container.AddComponent<object>("testmodelbinder");
+
+			var binder = new WindsorModelBinder(container);
+			binder.BindModel(_context);
+		}
+
+		public class TestModelBinder : IModelBinder
+		{
+			public ModelBinderResult BindModel(ModelBindingContext bindingContext)
+			{
+				return new ModelBinderResult("TestResult");
+			}
+		}
+	}
+}
