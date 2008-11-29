ffb231d6a2385a50cacc08b1488d733245f2ba1c
Jeremy.Skinner <Jeremy.Skinner@eb32e4ba-b140-0410-9c0e-b965c4cb1785>
2008-11-29 12:49:41 +0000

    Branch for grid refactoring.

---
+++ branches/GridRefactoring/src/MVCContrib/ConventionController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Linq.Expressions;
+using System.Web.Mvc;
+using MvcContrib.ActionResults;
+
+namespace MvcContrib
+{
+	/// <summary>
+	/// Base controller class that implements additional features over Controller.
+	/// </summary>
+	public abstract class ConventionController : Controller
+	{
+		/// <summary>
+		/// Creates a new instance of the ConventionController
+		/// </summary>
+		protected ConventionController() : this(new ConventionControllerActionInvoker())
+		{
+		}
+
+		/// <summary>
+		/// Creates a new instance of the ConventionController using the specified ActionInvoker.
+		/// </summary>
+		/// <param name="invokerToUse">The action invoker to use.</param>
+		protected ConventionController(IActionInvoker invokerToUse)
+		{
+			ActionInvoker = invokerToUse;
+		}
+
+		/// <summary>
+		/// Creates a <see cref="XmlResult"/> object for serializing the specified object as XML for writing to the HTTP Response's output stream.
+		/// </summary>
+		/// <param name="toSerialize">The object to serialize</param>
+		/// <returns>An XmlResult object</returns>
+		protected virtual XmlResult Xml(object toSerialize)
+		{
+			return new XmlResult(toSerialize);
+		}
+
+		/// <summary>
+		/// Creates a <see cref="BinaryResult"/> object for writing the specified binary content to the HTTP Response's output stream.
+		/// </summary>
+		/// <param name="content">The binary content.</param>
+		/// <param name="contentType">MIME type of the content</param>
+		/// <param name="asAttachment">Whether to specify the stream as an attachments. (When TRUE most bowsers will prompt the user to save the response instead of rendering in the browser.)</param>
+		/// <param name="filename">The filename to specify in the response.  (Most browsers will default to this value when saving as an attachment.) </param>
+		/// <returns>A BinaryResult object</returns>
+		protected virtual BinaryResult Binary(byte[] content, string contentType, bool asAttachment, string filename)
+		{
+			return new BinaryResult(content, contentType, asAttachment, filename);
+		}
+
+		/// <summary>
+		/// Redirects to an action on the same or another controller using expression-based syntax
+		/// </summary>
+		/// <typeparam name="T">The type of the controller on which to call the action</typeparam>
+		/// <param name="action">An expression which identifies the action to redirect to on the controller of type <typeparamref name="T"/></param>
+		/// <returns>A <see cref="RedirectToRouteResult"/> pointing to the action specified by the <paramref name="action"/> expression</returns>
+		protected virtual RedirectToRouteResult RedirectToAction<T>(Expression<Action<T>> action) where T : Controller
+		{
+			return ((Controller)this).RedirectToAction(action);
+		}
+	}
+}
---
+++ branches/GridRefactoring/src/MvcContrib.TestHelper/MvcContrib.TestHelper/RouteTestingExtensions.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Linq.Expressions;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using NUnit.Framework;
+using NUnit.Framework.SyntaxHelpers;
+using Rhino.Mocks;
+
+namespace MvcContrib.TestHelper
+{
+    /// <summary>
+    /// Used to simplify testing routes.
+    /// </summary>
+    public static class RouteTestingExtensions
+    {
+        private static HttpContextBase FakeHttpContext(string url)
+        {
+            var request = MockRepository.GenerateStub<HttpRequestBase>();            
+            request.Stub(x => x.AppRelativeCurrentExecutionFilePath).Return(url).Repeat.Any();
+            request.Stub(x => x.PathInfo).Return(string.Empty).Repeat.Any();
+
+            var context = MockRepository.GenerateStub<HttpContextBase>();
+            context.Stub(x => x.Request).Return(request).Repeat.Any();
+
+            return context;        
+        }
+
+        /// <summary>
+        /// Returns the corresponding route for the URL.  Returns null if no route was found.
+        /// </summary>
+        /// <param name="url">The app relative url to test.</param>
+        /// <returns>A matching <see cref="RouteData" />, or null.</returns>
+        public static RouteData Route(this string url)
+        {
+            var context = FakeHttpContext(url);
+            return RouteTable.Routes.GetRouteData(context);
+        }
+
+        /// <summary>
+        /// Asserts that the route matches the expression specified.  Checks controller, action, and any method arguments
+        /// into the action as route values.
+        /// </summary>
+        /// <typeparam name="TController">The controller.</typeparam>
+        /// <param name="routeData">The routeData to check</param>
+        /// <param name="action">The action to call on TController.</param>
+        public static RouteData ShouldMapTo<TController>(this RouteData routeData, Expression<Func<TController, ActionResult>> action)
+            where TController : Controller
+        {            
+            Assert.That(routeData, Is.Not.Null, "The URL did not match any route");
+
+            //check controller
+            routeData.ShouldMapTo<TController>();
+            
+            //check action
+            var methodCall = (MethodCallExpression) action.Body;
+            string actualAction = routeData.Values.GetValue("action").ToString();
+            string expectedAction = methodCall.Method.Name;
+            actualAction.AssertSameStringAs(expectedAction);
+                        
+            //check parameters
+            for (int i = 0; i < methodCall.Arguments.Count; i++)
+            {
+                string name = methodCall.Method.GetParameters()[i].Name;
+                object value = ((ConstantExpression) methodCall.Arguments[i]).Value;
+
+                Assert.That(routeData.Values.GetValue(name), Is.EqualTo(value.ToString()));
+            }
+
+            return routeData;
+        }
+
+        /// <summary>
+        /// Verifies the <see cref="RouteData">routeData</see> maps to the controller type specified.
+        /// </summary>
+        /// <typeparam name="TController"></typeparam>
+        /// <param name="routeData"></param>
+        /// <returns></returns>
+        public static RouteData ShouldMapTo<TController>(this RouteData routeData) where TController : Controller
+        {
+            //strip out the word 'Controller' from the type
+            string expected = typeof(TController).Name.Replace("Controller", "");
+
+            //get the key (case insensitive)
+            string actual = routeData.Values.GetValue("controller").ToString();
+
+            expected.AssertSameStringAs(actual);
+            return routeData;
+        }
+
+        /// <summary>
+        /// Gets a value from the <see cref="RouteValueDictionary" /> by key.  Does a
+        /// case-insensitive search on the keys.
+        /// </summary>
+        /// <param name="routeValues"></param>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public static object GetValue(this RouteValueDictionary routeValues, string key)
+        {
+            foreach(var routeValueKey in routeValues.Keys)
+            {
+                if(string.Equals(routeValueKey, key, StringComparison.InvariantCultureIgnoreCase))
+                    return routeValues[routeValueKey] as string;
+            }
+
+            return null;
+        }
+    }
+}
