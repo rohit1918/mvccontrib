63deb14ed52d451ffe2d92b39e1a1fd0d4c0ea7e
mvccontrib <mvccontrib@eb32e4ba-b140-0410-9c0e-b965c4cb1785>
2008-12-02 17:03:14 +0000

    CCNET build 187

---
+++ tags/187/src/Samples/MvcContrib.Samples.NinjectControllerFactory/NinjectControllerFactory.Domain/Samurai.cs
@@ -0,0 +1,17 @@
+﻿namespace NinjectControllerFactory.Domain
+{
+    public class Samurai
+    {
+        private readonly IWeapon _weapon;
+
+        public Samurai(IWeapon weapon)
+        {
+            _weapon = weapon;
+        }
+
+        public string Attack(string target)
+        {
+            return _weapon.Hit(target);
+        }
+    }
+}
