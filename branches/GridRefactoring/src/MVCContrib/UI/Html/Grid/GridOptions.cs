2a04ba042d5e01a7761b81d87c92c453cee6c3f5
Jeremy.Skinner <Jeremy.Skinner@eb32e4ba-b140-0410-9c0e-b965c4cb1785>
2008-12-13 19:32:34 +0000

    Moving GridOptions.LoadFromDictionary into an extension method in the legacy namespace

--- branches/GridRefactoring/src/MVCContrib/UI/Html/Grid/GridOptions.cs
+++ branches/GridRefactoring/src/MVCContrib/UI/Html/Grid/GridOptions.cs
@@ -7,15 +7,6 @@ namespace MvcContrib.UI.Html.Grid
 	/// </summary>
 	public class GridOptions
 	{
-		private const string Empty_Text_Key = "empty";
-		private const string Pagination_Format_Text_Key = "paginationFormat";
-		private const string Page_Query_Name_Text_Key = "page";
-		private const string Pagination_Single_Format_Text_Key = "paginationSingleFormat";
-		private const string Pagination_First_Text_Key = "first";
-		private const string Pagination_Prev_Text_Key = "prev";
-		private const string Pagination_Next_Text_Key = "next";
-		private const string Pagination_Last_Text_Key = "last";
-
 		public string EmptyMessageText { get; set; }
 		public string PaginationFormat { get; set; }
 		public string PaginationSingleFormat { get; set; }
@@ -24,16 +15,6 @@ namespace MvcContrib.UI.Html.Grid
 		public string PaginationNext { get; set; }
 		public string PaginationLast { get; set; }
 		public string PageQueryName { get; set; }
-
-		/// <summary>
-		/// Creates a new instance of the GridOptions class and will attempt to extract a default set of options from the specified dictionary.
-		/// This constructor is present for backwards compatibility with the Legacy Grid.
-		/// </summary>
-		/// <param name="options"></param>
-		public GridOptions(IDictionary options) : this()
-		{
-			LoadFromDictionary(options);
-		}
 
 		/// <summary>
 		/// Creates a new instance of the GridOptions class.
@@ -49,57 +30,6 @@ namespace MvcContrib.UI.Html.Grid
 			PaginationFormat = "Showing {0} - {1} of {2} ";
 		}
 
-		private void LoadFromDictionary(IDictionary options)
-		{
-			if(options == null) return;
-
-			if(options.Contains(Empty_Text_Key))
-			{
-				EmptyMessageText = options[Empty_Text_Key] as string;
-				options.Remove(Empty_Text_Key);
-			}
-
-			if(options.Contains(Pagination_Format_Text_Key))
-			{
-				PaginationFormat = options[Pagination_Format_Text_Key] as string;
-				options.Remove(Pagination_Format_Text_Key);
-			}
-
-			if(options.Contains(Pagination_Single_Format_Text_Key))
-			{
-				PaginationSingleFormat = options[Pagination_Single_Format_Text_Key] as string;
-				options.Remove(Pagination_Single_Format_Text_Key);
-			}
-
-			if(options.Contains(Pagination_First_Text_Key))
-			{
-				PaginationFirst = options[Pagination_First_Text_Key] as string;
-				options.Remove(Pagination_First_Text_Key);
-			}
-
-			if(options.Contains(Pagination_Prev_Text_Key))
-			{
-				PaginationPrev = options[Pagination_Prev_Text_Key] as string;
-				options.Remove(Pagination_Prev_Text_Key);
-			}
-
-			if(options.Contains(Pagination_Next_Text_Key))
-			{
-				PaginationNext = options[Pagination_Next_Text_Key] as string;
-				options.Remove(Pagination_Next_Text_Key);
-			}
-
-			if(options.Contains(Pagination_Last_Text_Key))
-			{
-				PaginationLast = options[Pagination_Last_Text_Key] as string;
-				options.Remove(Pagination_Last_Text_Key);
-			}
-
-			if(options.Contains(Page_Query_Name_Text_Key))
-			{
-				PageQueryName = options[Page_Query_Name_Text_Key] as string;
-				options.Remove(Page_Query_Name_Text_Key);
-			}
-		}
+		
 	}
 }
--- branches/GridRefactoring/src/MVCContrib/UI/Html/Grid/Legacy/GridHelper.cs
+++ branches/GridRefactoring/src/MVCContrib/UI/Html/Grid/Legacy/GridHelper.cs
@@ -11,6 +11,15 @@ namespace MvcContrib.UI.Html.Grid.Legacy
 	/// </summary>
 	public static class LegacyGridExtensions
 	{
+		private const string Empty_Text_Key = "empty";
+		private const string Pagination_Format_Text_Key = "paginationFormat";
+		private const string Page_Query_Name_Text_Key = "page";
+		private const string Pagination_Single_Format_Text_Key = "paginationSingleFormat";
+		private const string Pagination_First_Text_Key = "first";
+		private const string Pagination_Prev_Text_Key = "prev";
+		private const string Pagination_Next_Text_Key = "next";
+		private const string Pagination_Last_Text_Key = "last";
+
 		public static void Grid<T>(this HtmlHelper helper, string viewDataKey, Action<IRootGridColumnBuilder<T>> columns) where T : class
 		{
 			Grid(helper, viewDataKey, null, columns, null);
@@ -104,5 +113,50 @@ namespace MvcContrib.UI.Html.Grid.Legacy
 			return collection;
 		}
 
+		public static GridOptions LoadFromDictionary(this GridOptions options, IDictionary dictionary) {
+			if (dictionary == null) return options;
+
+			if (dictionary.Contains(Empty_Text_Key)) {
+				options.EmptyMessageText = dictionary[Empty_Text_Key] as string;
+				dictionary.Remove(Empty_Text_Key);
+			}
+
+			if (dictionary.Contains(Pagination_Format_Text_Key)) {
+				options.PaginationFormat = dictionary[Pagination_Format_Text_Key] as string;
+				dictionary.Remove(Pagination_Format_Text_Key);
+			}
+
+			if (dictionary.Contains(Pagination_Single_Format_Text_Key)) {
+				options.PaginationSingleFormat = dictionary[Pagination_Single_Format_Text_Key] as string;
+				dictionary.Remove(Pagination_Single_Format_Text_Key);
+			}
+
+			if (dictionary.Contains(Pagination_First_Text_Key)) {
+				options.PaginationFirst = dictionary[Pagination_First_Text_Key] as string;
+				dictionary.Remove(Pagination_First_Text_Key);
+			}
+
+			if (dictionary.Contains(Pagination_Prev_Text_Key)) {
+				options.PaginationPrev = dictionary[Pagination_Prev_Text_Key] as string;
+				dictionary.Remove(Pagination_Prev_Text_Key);
+			}
+
+			if (dictionary.Contains(Pagination_Next_Text_Key)) {
+				options.PaginationNext = dictionary[Pagination_Next_Text_Key] as string;
+				dictionary.Remove(Pagination_Next_Text_Key);
+			}
+
+			if (dictionary.Contains(Pagination_Last_Text_Key)) {
+				options.PaginationLast = dictionary[Pagination_Last_Text_Key] as string;
+				dictionary.Remove(Pagination_Last_Text_Key);
+			}
+
+			if (dictionary.Contains(Page_Query_Name_Text_Key)) {
+				options.PageQueryName = dictionary[Page_Query_Name_Text_Key] as string;
+				dictionary.Remove(Page_Query_Name_Text_Key);
+			}
+
+			return options;
+		}
 	}
 }
