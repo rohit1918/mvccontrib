b0536629011300db9d765fc8273143974ead1c39
Jeremy.Skinner <Jeremy.Skinner@eb32e4ba-b140-0410-9c0e-b965c4cb1785>
2009-01-28 10:04:19 +0000

    Fixing FormHelper and marking the class as obsolete. FormHelper will be removed with the next release.

--- branches/rc/src/MvcContrib.BrailViewEngine/BrailBase.cs
+++ branches/rc/src/MvcContrib.BrailViewEngine/BrailBase.cs
@@ -414,7 +414,7 @@ namespace MvcContrib.BrailViewEngine
 
 			properties["html"] = new HtmlHelper(viewContext, this);
 			properties["url"] = new UrlHelper(viewContext.RequestContext);
-			properties["form"] = new FormHelper() { ViewContext = viewContext };
+			//properties["form"] = new FormHelper() { ViewContext = viewContext };
 
 //			if (controllerContext.Resources != null)
 //			{
