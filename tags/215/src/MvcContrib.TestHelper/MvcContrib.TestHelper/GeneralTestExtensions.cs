8c8a63b3c9777c2c0ba96dc5b061da63ec2af4ff
mvccontrib <mvccontrib@eb32e4ba-b140-0410-9c0e-b965c4cb1785>
2009-01-29 03:35:05 +0000

    CCNET build 215

---
+++ tags/215/src/MvcContrib.TestHelper/MvcContrib.TestHelper/GeneralTestExtensions.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using NUnit.Framework;
+using NUnit.Framework.SyntaxHelpers;
+
+namespace MvcContrib.TestHelper
+{
+    /// <summary>
+    /// Contains basic extension methods to simplify testing.
+    /// </summary>
+    public static class GeneralTestExtensions
+    {
+        /// <summary>
+        /// Asserts that the object is of type T.  Also returns T to enable method chaining.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="actual"></param>
+        /// <returns></returns>
+        public static T ShouldBe<T>(this object actual)
+        {
+            Assert.That(actual, Is.InstanceOfType(typeof(T)));
+            return (T)actual;
+        }
+
+        /// <summary>
+        /// Asserts that the object is the expected value.
+        /// </summary>
+        /// <param name="actual"></param>
+        /// <param name="expected"></param>
+        public static void ShouldBe(this object actual, object expected)
+        {
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+
+        /// <summary>
+        /// Compares the two strings (case-insensitive).
+        /// </summary>
+        /// <param name="left"></param>
+        /// <param name="right"></param>
+        public static void AssertSameStringAs(this string left, string right)
+        {
+            Assert.That(string.Equals(left, right, StringComparison.InvariantCultureIgnoreCase),
+                string.Format("Expected {0} but was {1}", right, left));
+        }
+    }
+}
