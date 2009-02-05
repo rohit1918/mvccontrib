5772e91428687310125a523f6f2fcfad1888106a
Jeremy.Skinner <Jeremy.Skinner@eb32e4ba-b140-0410-9c0e-b965c4cb1785>
2009-02-05 15:02:42 +0000

    Minor refactoring of GridRenderer

--- branches/RC1Grid/src/MVCContrib/UI/Grid/HtmlTableGridRenderer.cs
+++ branches/RC1Grid/src/MVCContrib/UI/Grid/HtmlTableGridRenderer.cs
@@ -10,49 +10,59 @@ namespace MvcContrib.UI.Grid
 	{
 		private const string DefaultCssClass = "grid";
 
-		protected override void RenderHeaderCellEnd() {
+		protected override void RenderHeaderCellEnd()
+		{
 			RenderText("</th>");
 		}
 
-		protected override void RenderHeaderCellStart(GridColumn<T> column) {
+		protected override void RenderHeaderCellStart(GridColumn<T> column)
+		{
 			string attrs = BuildHtmlAttributes(column.HeaderAttributes);
-			if (attrs.Length > 0)
+			if(attrs.Length > 0)
 				attrs = " " + attrs;
 
 			RenderText(string.Format("<th{0}>", attrs));
 		}
 
-		protected override void RenderRowStart(bool isAlternate) {
-			if (isAlternate) {
+		protected override void RenderRowStart(bool isAlternate)
+		{
+			if(isAlternate)
+			{
 				RenderText("<tr class=\"gridrow_alternate\">");
 			}
-			else {
+			else
+			{
 				RenderText("<tr class=\"gridrow\">");
 			}
 		}
 
-		protected override void RenderRowEnd() {
+		protected override void RenderRowEnd()
+		{
 			RenderText("</tr>");
 		}
 
-		protected override void RenderEndCell() {
+		protected override void RenderEndCell()
+		{
 			RenderText("</td>");
 		}
 
-		protected override void RenderStartCell(GridColumn<T> column) {
+		protected override void RenderStartCell(GridColumn<T> column)
+		{
 			RenderText("<td>");
 		}
 
-		protected override void RenderHeadStart() {
+		protected override void RenderHeadStart()
+		{
 			RenderText("<thead><tr>");
 		}
 
-		protected override void RenderHeadEnd() {
+		protected override void RenderHeadEnd()
+		{
 			RenderText("</tr></thead>");
 		}
 
-		protected override void RenderGridStart() {
-
+		protected override void RenderGridStart()
+		{
 			if(! GridModel.Attributes.ContainsKey("class"))
 			{
 				GridModel.Attributes["class"] = DefaultCssClass;
@@ -60,27 +70,28 @@ namespace MvcContrib.UI.Grid
 
 			string attrs = BuildHtmlAttributes(GridModel.Attributes);
 
-			if (attrs.Length > 0)
+			if(attrs.Length > 0)
 				attrs = " " + attrs;
 
 			RenderText(string.Format("<table{0}>", attrs));
 		}
 
-		protected override void RenderGridEnd(bool isEmpty) {
+		protected override void RenderGridEnd(bool isEmpty)
+		{
 			RenderText("</table>");
 		}
 
-		protected override void RenderEmpty() {
+		protected override void RenderEmpty()
+		{
 			RenderText("<tr><td>" + GridModel.EmptyText + "</td></tr>");
 		}
-
 
 		/// <summary>
 		/// Converts the specified attributes dictionary of key-value pairs into a string of HTML attributes. 
 		/// </summary>
 		/// <returns></returns>
-		private static string BuildHtmlAttributes(IDictionary<string, object> attributes) {
-
+		private static string BuildHtmlAttributes(IDictionary<string, object> attributes)
+		{
 			if(attributes == null || attributes.Count == 0)
 			{
 				return string.Empty;
@@ -89,8 +100,8 @@ namespace MvcContrib.UI.Grid
 			const string attributeFormat = "{0}=\"{1}\"";
 
 			return string.Join(" ",
-			                   attributes.Select(pair => string.Format(attributeFormat, pair.Key, pair.Value)).ToArray()
-				);
+                   attributes.Select(pair => string.Format(attributeFormat, pair.Key, pair.Value)).ToArray()
+			);
 		}
 	}
 }
