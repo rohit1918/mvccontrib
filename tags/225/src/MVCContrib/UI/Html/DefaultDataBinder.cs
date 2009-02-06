3ae82dcbeb2a69b50922e33f77878ee53d182a79
mvccontrib <mvccontrib@eb32e4ba-b140-0410-9c0e-b965c4cb1785>
2009-02-06 05:44:48 +0000

    CCNET build 225

---
+++ tags/225/src/MVCContrib/UI/Html/DefaultDataBinder.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Web.Mvc;
+
+namespace MvcContrib.UI.Html
+{
+	[Obsolete]
+	public class DefaultDataBinder : IDataBinder
+	{
+		public object NestedRootInstance { get; set; }
+        
+		public object ExtractValue(string target, ViewContext context)
+		{
+			if (target == null)
+				return null;
+
+			var viewData = context.ViewData;
+
+			if(NestedRootInstance != null)
+			{
+				viewData = new ViewDataDictionary(NestedRootInstance);
+			}
+
+			return viewData.Eval(target);
+		}
+
+		public IDisposable NestedBindingScope(object rootDataItem)
+		{
+			return new DefaultBindingScope(this, rootDataItem);
+		}
+
+		public class DefaultBindingScope : IDisposable
+		{
+			private readonly DefaultDataBinder _binder;
+			private readonly object _originalRootInstance;
+
+			public DefaultBindingScope(DefaultDataBinder binder, object newRootInstance)
+			{
+				_binder = binder;
+				_originalRootInstance = binder.NestedRootInstance;
+				binder.NestedRootInstance = newRootInstance;
+			}
+
+			public void Dispose()
+			{
+				_binder.NestedRootInstance = _originalRootInstance;
+			}
+		}
+	}
+}
---
+++ tags/225/src/MvcContrib.FluentHtml/Elements/FormElement.cs
@@ -0,0 +1,76 @@
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Web.Mvc;
+using MvcContrib.FluentHtml.Behaviors;
+using MvcContrib.FluentHtml.Html;
+
+namespace MvcContrib.FluentHtml.Elements
+{
+	/// <summary>
+	/// Base class for form elements.
+	/// </summary>
+	/// <typeparam name="T">Derived type</typeparam>
+	public abstract class FormElement<T> : DisableableElement<T>, IMemberElement where T : FormElement<T>, IElement
+	{
+		protected MemberExpression forMember;
+		private readonly IEnumerable<IMemberBehavior> behaviors;
+
+		protected FormElement(string tag, string name, MemberExpression forMember, IEnumerable<IMemberBehavior> behaviors)
+			: this(tag, name)
+		{
+			this.forMember = forMember;
+			this.behaviors = behaviors;
+		}
+
+		protected FormElement(string tag, string name) : base(tag)
+		{
+			builder.MergeAttribute(HtmlAttribute.Name, name, true);
+		}
+
+		/// <summary>
+		/// Expression indicating the view model member assocaited with the element.</param>
+		/// </summary>
+		public virtual MemberExpression ForMember
+		{
+			get { return forMember; }
+		}
+
+		public override string ToString()
+		{
+			InferIdFromName();
+			ApplyBehaviors();
+			PreRender();
+			return base.ToString();
+		}
+
+		protected virtual void PreRender() { }
+
+		/// <summary>
+		/// Determines how the HTML element is closed.
+		/// </summary>
+		public override TagRenderMode TagRenderMode
+		{
+			get { return TagRenderMode.SelfClosing; }
+		}
+
+		protected void ApplyBehaviors()
+		{
+			if(behaviors == null)
+			{
+				return;
+			}
+			foreach(var behavior in behaviors)
+			{
+				behavior.Execute(this);
+			}
+		}
+
+		protected virtual void InferIdFromName()
+		{
+			if (!builder.Attributes.ContainsKey(HtmlAttribute.Id))
+			{
+				Attr(HtmlAttribute.Id, builder.Attributes[HtmlAttribute.Name].GenerateHtmlId());
+			}
+		}
+	}
+}
