f4a09c0fc3658561eae8a8df68822c49d9b76d67
tscott@lunaversesoftware.com <tscott@lunaversesoftware.com@eb32e4ba-b140-0410-9c0e-b965c4cb1785>
2009-02-17 16:30:53 +0000

    - Modified StringExtensions.FormatAsHtmlId to replace all characters not valid for ID attribute with underscore - Modofied RadioButtonBase.InferIdFromName to apply FormatAsHtmlId to element value as well.

--- src/MvcContrib.FluentHtml/StringExtensions.cs
+++ src/MvcContrib.FluentHtml/StringExtensions.cs
@@ -1,12 +1,16 @@
+using System.Text.RegularExpressions;
+
 namespace MvcContrib.FluentHtml
 {
 	public static class StringExtensions
 	{
 		public static string FormatAsHtmlId(this string name)
 		{
+			//Replace charactes not valid for ID attribute with underscores.
+			//Replace dots with underscores to distinguish from name attribute.
 			return string.IsNullOrEmpty(name) 
 				? string.Empty 
-				: name.Replace(' ', '_').Replace('.', '_');
+				: Regex.Replace(name, "[^a-zA-Z0-9-:]", "_");
 		}
 	}
 }
