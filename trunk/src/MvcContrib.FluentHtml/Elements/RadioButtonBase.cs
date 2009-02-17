532a95c4160bb0ee6bc3528fd5c5726193b5f483
tscott@lunaversesoftware.com <tscott@lunaversesoftware.com@eb32e4ba-b140-0410-9c0e-b965c4cb1785>
2009-02-17 16:30:53 +0000

    - Modified StringExtensions.FormatAsHtmlId to replace all characters not valid for ID attribute with underscore - Modofied RadioButtonBase.InferIdFromName to apply FormatAsHtmlId to element value as well.

--- trunk/src/MvcContrib.FluentHtml/Elements/RadioButtonBase.cs
+++ trunk/src/MvcContrib.FluentHtml/Elements/RadioButtonBase.cs
@@ -48,10 +48,10 @@ namespace MvcContrib.FluentHtml.Elements
 			if (!builder.Attributes.ContainsKey(HtmlAttribute.Id))
 			{
 				Attr(HtmlAttribute.Id, string.Format("{0}{1}",
-					builder.Attributes[HtmlAttribute.Name].FormatAsHtmlId(),
+					builder.Attributes[HtmlAttribute.Name],
 					elementValue == null 
-						? null 
-						: string.Format("_{0}", elementValue)));
+						? null
+						: string.Format("_{0}", elementValue)).FormatAsHtmlId());
 			}
 		}
 
