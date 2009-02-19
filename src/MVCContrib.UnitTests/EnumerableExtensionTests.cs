e0bd53755edc3e27ad8ecba0eca085833a4979e3
Jeremy.Skinner <Jeremy.Skinner@eb32e4ba-b140-0410-9c0e-b965c4cb1785>
2009-02-19 16:15:09 +0000

    New implementation of ToSelectList as discussed at http://groups.google.com/group/mvccontrib-discuss/browse_thread/thread/fab5d96117a6210

--- src/MVCContrib.UnitTests/EnumerableExtensionTests.cs
+++ src/MVCContrib.UnitTests/EnumerableExtensionTests.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Web.Mvc;
 using MvcContrib.EnumerableExtensions;
 using NUnit.Framework;
 using NUnit.Framework.SyntaxHelpers;
@@ -11,23 +12,36 @@ namespace MvcContrib.UnitTests
 		[Test]
 		public void Should_create_select_list_with_value_and_text_field()
 		{
-			var list = People.ToSelectList(x => x.Id, x => x.Name);
-			Assert.That(list.DataTextField, Is.EqualTo("Name"));
-			Assert.That(list.DataValueField, Is.EqualTo("Id"));
+			var sequence = People.ToSelectList(x => x.Id, x => x.Name);
+			Assert.That(sequence.Count(), Is.EqualTo(3));
+
+			Assert.That(sequence.ElementAt(1).Value, Is.EqualTo("2"));
+			Assert.That(sequence.ElementAt(1).Text, Is.EqualTo("Eric"));
+
+			Assert.That(sequence.Last().Value, Is.EqualTo("3"));
+			Assert.That(sequence.Last().Text, Is.EqualTo("Jeremy"));
 		}
 
 		[Test]
 		public void Should_create_select_list_with_selected_value()
 		{
-			var list = People.ToSelectList(x => x.Id, x => x.Name, 3);
-			Assert.That(list.SelectedValue, Is.EqualTo(3));
+			var sequence = People.ToSelectList(x => x.Id, x => x.Name, new[] { 3 });
+			Assert.That(sequence.Last().Selected, Is.True);
 		}
 
 		[Test]
 		public void Should_create_select_list_with_selected_value_using_selector()
 		{
-			var list = People.ToSelectList(x => x.Id, x => x.Name, x => x.Name == "Jeremy");
-			Assert.That(list.SelectedValues.Cast<Person>().Single().Id, Is.EqualTo(3));
+			var sequence = People.ToSelectList(x => x.Id, x => x.Name, x => x.Name == "Jeremy");
+			Assert.That(sequence.Last().Selected, Is.True);
+		}
+
+		[Test]
+		public void Should_not_throw_when_value_field_is_null()
+		{
+			var people = new List<Person> {new Person {Id = 1, Name = null}};
+			var sequence = people.ToSelectList(x => x.Id, x => x.Name, (IEnumerable<int>)null);
+			Assert.That(sequence.Count(), Is.EqualTo(1));
 		}
 
 		private IEnumerable<Person> People
