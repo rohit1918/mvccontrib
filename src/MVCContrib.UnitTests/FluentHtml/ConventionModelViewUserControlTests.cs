5425f9addd5c30bcc6d1e836ce5a2a5b589563a5
Jeremy.Skinner <Jeremy.Skinner@eb32e4ba-b140-0410-9c0e-b965c4cb1785>
2009-02-21 12:01:33 +0000

    Fixing ConventionModelViewUserControlTests

--- src/MVCContrib.UnitTests/FluentHtml/ConventionModelViewUserControlTests.cs
+++ src/MVCContrib.UnitTests/FluentHtml/ConventionModelViewUserControlTests.cs
@@ -5,7 +5,7 @@ using NUnit.Framework;
 namespace MvcContrib.UnitTests.FluentHtml
 {
 	[TestFixture]
-	public class ConventionModelViewUserControlTests : ConventionViewModelContainerTestBase<ConventionModelViewUserContol<FakeModel>, FakeModel>
+	public class ConventionModelViewUserControlTests : ConventionViewModelContainerTestBase<ConventionModelViewUserControl<FakeModel>, FakeModel>
 	{
 	}
 }
