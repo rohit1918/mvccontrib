20b2fd562143a3aacb31713fe00f086cba349833
mvccontrib <mvccontrib@eb32e4ba-b140-0410-9c0e-b965c4cb1785>
2009-03-03 23:01:44 +0000

    CCNET build 247

---
+++ tags/247/src/MVCContrib/UI/Grid/ColumnBuilder.cs
@@ -0,0 +1,105 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace MvcContrib.UI.Grid
+{
+	/// <summary>
+	/// Builds grid columns
+	/// </summary>
+	public class ColumnBuilder<T> : ICollection<GridColumn<T>> where T : class 
+	{
+		private readonly List<GridColumn<T>> _columns = new List<GridColumn<T>>();
+
+		/// <summary>
+		/// Specifies a column should be constructed for the specified property.
+		/// </summary>
+		/// <param name="propertySpecifier">Lambda that specifies the property for which a column should be constructed</param>
+		public IGridColumn<T> For(Expression<Func<T, object>> propertySpecifier)
+		{
+			string inferredName = ExpressionToName(propertySpecifier);
+			var column = new GridColumn<T>(propertySpecifier.Compile(), inferredName);
+			_columns.Add(column);
+			return column;
+		}
+
+		/// <summary>
+		/// Specifies that a custom column should be constructed with the specified name.
+		/// </summary>
+		/// <param name="name"></param>
+		public IGridColumn<T> For(string name) 
+		{
+			var column = new GridColumn<T>(x => string.Empty, name);
+			_columns.Add(column);
+			return column.Partial(name);
+		}
+
+		public IEnumerator<GridColumn<T>> GetEnumerator()
+		{
+			return _columns.GetEnumerator();
+		}
+
+		IEnumerator IEnumerable.GetEnumerator()
+		{
+			return GetEnumerator();
+		}
+
+		/// <summary>
+		/// Grabs the property name from a member expression.
+		/// </summary>
+		/// <param name="expression">The expression</param>
+		/// <returns>The name of the property</returns>
+		public static string ExpressionToName<TProperty>(Expression<Func<T, TProperty>> expression)
+		{
+			var memberExpression = RemoveUnary(expression.Body) as MemberExpression;
+			return memberExpression == null ? null : memberExpression.Member.Name;
+		}
+
+
+		private static Expression RemoveUnary(Expression body)
+		{
+			var unary = body as UnaryExpression;
+			if(unary != null)
+			{
+				return unary.Operand;
+			}
+			return body;
+		}
+
+		void ICollection<GridColumn<T>>.Add(GridColumn<T> column)
+		{
+			_columns.Add(column);
+		}
+
+		void ICollection<GridColumn<T>>.Clear()
+		{
+			_columns.Clear();
+		}
+
+		bool ICollection<GridColumn<T>>.Contains(GridColumn<T> column)
+		{
+			return _columns.Contains(column);
+		}
+
+		void ICollection<GridColumn<T>>.CopyTo(GridColumn<T>[] array, int arrayIndex)
+		{
+			_columns.CopyTo(array, arrayIndex);
+		}
+
+		bool ICollection<GridColumn<T>>.Remove(GridColumn<T> column)
+		{
+			return _columns.Remove(column);
+		}
+
+		int ICollection<GridColumn<T>>.Count
+		{
+			get { return _columns.Count; }
+		}
+
+		bool ICollection<GridColumn<T>>.IsReadOnly
+		{
+			get { return false; }
+		}
+	}
+}
---
+++ tags/247/src/MvcContrib.FluentHtml/Elements/SelectBase.cs
@@ -0,0 +1,69 @@
+using System.Linq.Expressions;
+using System.Text;
+using System.Web.Mvc;
+using System.Collections.Generic;
+using MvcContrib.FluentHtml.Behaviors;
+using MvcContrib.FluentHtml.Html;
+
+namespace MvcContrib.FluentHtml.Elements
+{
+	/// <summary>
+	/// Base class for select elements.
+	/// </summary>
+	/// <typeparam name="T">The derived type.</typeparam>
+	public abstract class SelectBase<T> : OptionsElementBase<T> where T : SelectBase<T>
+	{
+		protected SelectBase(string name) : base(HtmlTag.Select, name) { }
+
+		protected SelectBase(string name, MemberExpression forMember, IEnumerable<IBehaviorMarker> behaviors)
+			: base(HtmlTag.Select, name, forMember, behaviors) { }
+
+		/// <summary>
+		/// Set the 'size' attribute.
+		/// </summary>
+		/// <param name="value">The value of the 'size' attribute.</param>
+		/// <returns></returns>
+		public virtual T Size(int value)
+		{
+			Attr(HtmlAttribute.Size, value);
+			return (T)this;
+		}
+
+		protected override void PreRender()
+		{
+			builder.InnerHtml = RenderOptions();
+			base.PreRender();
+		}
+
+		public override TagRenderMode TagRenderMode
+		{
+			get { return TagRenderMode.Normal; }
+		}
+
+		private string RenderOptions()
+		{
+			if (_options == null)
+			{
+				return null;
+			}
+
+			var sb = new StringBuilder();
+			foreach (var options in _options)
+			{
+				sb.Append(GetOption(options));
+			}
+			return sb.ToString();
+		}
+
+		protected virtual Option GetOption(object option)
+		{
+			var value = _valueFieldSelector(option);
+			var text = _textFieldSelector(option);
+
+			return new Option()
+				.Value(value == null ? string.Empty : value.ToString())
+				.Text(text == null ? string.Empty : text.ToString())
+				.Selected(IsSelectedValue(value));
+		}
+	}
+}
