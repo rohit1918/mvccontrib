2327604d111d6f4e053e493cc3a68080d15c02f8
mvccontrib <mvccontrib@eb32e4ba-b140-0410-9c0e-b965c4cb1785>
2009-03-03 22:47:33 +0000

    CCNET build 242

---
+++ tags/242/src/MVCContrib.UnitTests/FluentHtml/LiteralTests.cs
@@ -0,0 +1,47 @@
+using System.Web;
+using MvcContrib.UnitTests.FluentHtml.Helpers;
+using NUnit.Framework;
+using MvcContrib.FluentHtml.Elements;
+using MvcContrib.FluentHtml.Html;
+
+namespace MvcContrib.UnitTests.FluentHtml
+{
+	[TestFixture]
+	public class LiteralTests
+	{
+		[Test]
+		public void literal_renders_with_correct_tag()
+		{
+			var html = new Literal().ToString();
+			html.ShouldRenderHtmlDocument().ChildNodes[0]
+				.ShouldBeNamed(HtmlTag.Span);
+		}
+
+		[Test]
+		public void literal_renders_with_correct_inner_text()
+		{
+			var html = new Literal().Value("foo bar").ToString();
+			html.ShouldRenderHtmlDocument().ChildNodes[0]
+				.ShouldHaveInnerTextEqual("foo bar");
+		}
+
+		[Test]
+		public void literal_renders_with_inner_text_formatted()
+		{
+			var item = 1234.5m;
+			var expected = string.Format("{0:$#,##0.00}", item);
+			var html = new Literal().Value(item).Format("$#,##0.00").ToString();
+			html.ShouldRenderHtmlDocument().ChildNodes[0]
+				.ShouldHaveInnerTextEqual(expected);
+		}
+
+        [Test]
+        public void textarea_value_html_encodes_value()
+        {
+            var value = "<div>Foo</div>";
+            var html = new Literal().Value(value).ToString();
+            html.ShouldRenderHtmlDocument().ChildNodes[0]
+                .ShouldHaveInnerTextEqual(HttpUtility.HtmlEncode(value));
+        }
+	}
+}
---
+++ tags/242/src/MvcContrib.FluentHtml/IViewModelContainer.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic;
+using System.Web.Mvc;
+using MvcContrib.FluentHtml.Behaviors;
+
+namespace MvcContrib.FluentHtml
+{
+	public interface IViewModelContainer<T> : IViewDataContainer where T : class
+	{
+		T ViewModel { get; }
+		IEnumerable<IBehaviorMarker> Behaviors { get; }
+		string HtmlNamePrefix { get; set;  }
+	}
+}
