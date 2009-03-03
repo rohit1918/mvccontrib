a786661bfe4fe6f9e6feb6661daa08229be90c31
mvccontrib <mvccontrib@eb32e4ba-b140-0410-9c0e-b965c4cb1785>
2009-03-03 22:58:44 +0000

    CCNET build 246

---
+++ tags/246/src/MvcContrib.FluentHtml/Elements/IElement.cs
@@ -0,0 +1,28 @@
+using System.Web.Mvc;
+
+namespace MvcContrib.FluentHtml.Elements
+{
+	/// <summary>
+	/// Interface for elements.
+	/// </summary>
+	public interface IElement
+	{
+		/// <summary>
+		/// TagBuilder object used to generate HTML.
+		/// </summary>
+		TagBuilder Builder { get; }
+
+		/// <summary>
+		/// Set the value of the specified attribute.
+		/// </summary>
+		/// <param name="name">The name of the attribute.</param>
+		/// <param name="value">The value of the attribute.</param>
+		void SetAttr(string name, object value);
+
+		/// <summary>
+		/// Set the value of the specified attribute.
+		/// </summary>
+		/// <param name="name">The name of the attribute.</param>
+		string GetAttr(string name);
+	}
+}
---
+++ tags/246/src/MvcContrib.FluentHtml/StringExtensions.cs
@@ -0,0 +1,24 @@
+using System.Text.RegularExpressions;
+
+namespace MvcContrib.FluentHtml
+{
+	public static class StringExtensions
+	{
+		public static string FormatAsHtmlId(this string name)
+		{
+			//Replace charactes not valid for ID attribute with underscores.
+			//Replace dots with underscores to distinguish from name attribute.
+			return string.IsNullOrEmpty(name) 
+				? string.Empty 
+				: Regex.Replace(name, "[^a-zA-Z0-9-:]", "_");
+		}
+
+		public static string FormatAsHtmlName(this string name)
+		{
+			//Replace charactes not valid for name attribute with underscores.
+			return string.IsNullOrEmpty(name)
+				? string.Empty
+				: Regex.Replace(name, "[^a-zA-Z0-9.-:]", "_");
+		}
+	}
+}
