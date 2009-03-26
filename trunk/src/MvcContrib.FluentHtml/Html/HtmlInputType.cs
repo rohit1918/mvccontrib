9cd07bdb86f07f1b551f940e22bb45b357fe78cf
subdigital <subdigital@eb32e4ba-b140-0410-9c0e-b965c4cb1785>
2009-03-26 16:30:18 +0000

    added Html.Button() and Html.ResetButton() from patch submitted by Jon Kruger.

--- trunk/src/MvcContrib.FluentHtml/Html/HtmlInputType.cs
+++ trunk/src/MvcContrib.FluentHtml/Html/HtmlInputType.cs
@@ -9,5 +9,7 @@ namespace MvcContrib.FluentHtml.Html
 		public const string Submit = "submit";
 		public const string File = "file";
 		public static string Radio = "radio";
+	    public const string Button = "button";
+	    public const string Reset = "reset";
 	}
 }
