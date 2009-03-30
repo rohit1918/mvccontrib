e20ee0fb6c99b90ec18d090c3a7b6398c598a2f8
Jeremy.Skinner <Jeremy.Skinner@eb32e4ba-b140-0410-9c0e-b965c4cb1785>
2009-03-30 10:45:35 +0000

    Issue #4138 - Getting DataList a bit more aligned with System.Web.UI.WebControls.DataList
    Fixed: Was only working if amount of columns and rows were the same when RepeatDirection was set to Vertical.
    Removed: DataListOptions class (not used anymore)
    Removed: Some Constructors on DataList to make it less complex (all options can be set using the fluent methods)
    Changed: Helper Method now used the DataList default settings.
    By Default it has one column and the RepeatDirection Vertical.
    You can set the amount of columns with the NumberOfColumns(int) method
    You can set the RepeatDirection with RepeatHorizontally() or RepeatVertically() methods

--- trunk/src/MVCContrib/UI/DataList/DataListExtensions.cs
+++ trunk/src/MVCContrib/UI/DataList/DataListExtensions.cs
@@ -1,20 +1,37 @@
 using System;
 using System.Collections.Generic;
 using System.Web.Mvc;
-using System.Web.UI.WebControls;
+using MvcContrib.UI.DataList;
 
 namespace MvcContrib.UI.DataList
 {
 	public static class DataListExtensions
 	{
-		public static DataList<T> DataList<T>(this HtmlHelper helper, IEnumerable<T> dataSource, params Func<object, object>[] tableAttributes)
-		{
-			DataList<T> list = new DataList<T>(dataSource, 3, RepeatDirection.Vertical, helper.ViewContext.HttpContext.Response.Output, new Hash(tableAttributes));
-			return list;
-		}
+		/// <summary>
+		/// Create a DataList.
+		/// </summary>
+		/// <typeparam name="T"></typeparam>
+		/// <param name="helper">The helper.</param>
+		/// <param name="dataSource">The data source.</param>
+		/// <returns></returns>
 		public static DataList<T> DataList<T>(this HtmlHelper helper, IEnumerable<T> dataSource)
 		{
-			DataList<T> list = new DataList<T>(dataSource, 3, RepeatDirection.Vertical, helper.ViewContext.HttpContext.Response.Output);
+			var list = new DataList<T>(dataSource, helper.ViewContext.HttpContext.Response.Output);
+			return list;
+		}
+
+		/// <summary>
+		/// Create a DataList.
+		/// </summary>
+		/// <typeparam name="T"></typeparam>
+		/// <param name="helper">The helper.</param>
+		/// <param name="dataSource">The data source.</param>
+		/// <param name="tableAttributes">The table attributes.</param>
+		/// <returns></returns>
+		public static DataList<T> DataList<T>(this HtmlHelper helper, IEnumerable<T> dataSource,
+		                                      params Func<object, object>[] tableAttributes)
+		{
+			var list = new DataList<T>(dataSource, helper.ViewContext.HttpContext.Response.Output, new Hash(tableAttributes));
 			return list;
 		}
 	}
